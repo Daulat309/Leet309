7f8e7916d3da4ec5f544bd73ab3ea1830cce2047
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2025-07-27 16:21:37 +0530

    Time: 1 ms (33.4%), Space: 41.5 MB (81.97%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
@@ -1,29 +1,32 @@
 class Solution {
-    public int l(int[] arr,int j,int k){
-        for(int i = j-1;i>=0;i--){
-            if(arr[i]!=k) return arr[i];
-        }
-        return k;
-    }
-    public int r(int[] arr,int j,int k){
-        for(int i = j+1;i<arr.length;i++){
-            if(arr[i]!=k) return arr[i];
-        }
-        return k;
-    }
+    // public int l(int[] arr,int j,int k){
+    //     for(int i = j-1;i>=0;i--){
+    //         if(arr[i]!=k) return arr[i];
+    //     }
+    //     return k;
+    // }
+    // public int r(int[] arr,int j,int k){
+    //     for(int i = j+1;i<arr.length;i++){
+    //         if(arr[i]!=k) return arr[i];
+    //     }
+    //     return k;
+    // }
     public int countHillValley(int[] arr) {
         int count = 0;
         for(int i = 1;i<arr.length-1;i++){
-            int prev = arr[i-1];
-            if(prev == arr[i]){
-                prev = l(arr,i,arr[i]);
+            if(arr[i] == arr[i-1]) continue;
+            int prev = i-1;
+            int next = i+1;
+            while(prev >= 0 && arr[i] == arr[prev]) {
+                prev--;
             }
-            int next = arr[i+1];
-            if(next==arr[i]){
-                next = r(arr,i,arr[i]);
+            while(next < arr.length && arr[i] == arr[next]) {
+                next++;
             }
-            if((arr[i]<prev&&arr[i]<next&&(arr[i]!=arr[i-1]))||(arr[i]>prev&&arr[i]>next&&(arr[i]!=arr[i-1]))){
-                count++;
+            if(prev >= 0 && next < arr.length){
+                if((arr[i]<arr[prev]&&arr[i]<arr[next])||(arr[i]>arr[prev]&&arr[i]>arr[next])){
+                    count++;
+                }
             }
         }
         return count;
