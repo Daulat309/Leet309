d2f46fccf1b973db4841a6e7df69f8566585f694
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2025-08-14 21:30:45 +0530

    Time: 2 ms (81.68%), Space: 42 MB (71.39%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
@@ -1,30 +1,12 @@
 class Solution {
     public String largestGoodInteger(String num) {
-        int first = num.charAt(0) - '0';
         int max = -1;
-        // if(num.length()<3) return "";
-        // if(num.length()==3){
-        //     if(num.charAt(0)==num.charAt(1)==num.charAt(2)) return num;
-        //     else return "";
-        // }
-        // if(num.length()==3){
-        //     if(num.charAt(0)==num.charAt(1)==num.charAt(2)) return num;
-        // }
-
-        for(int i = 1;i < num.length()-1;i++){
-            int second = num.charAt(i) - '0';
-            int third = num.charAt(i+1) - '0';
-            if(first==second&&first==third){
-                if(max<first) max = first;
-                if(i+2<num.length())first = num.charAt(i+2) - '0';
+        for (int i = 0; i <= num.length() - 3; i++) {
+            if (num.charAt(i) == num.charAt(i + 1) && num.charAt(i) == num.charAt(i + 2)) {
+                max = Math.max(max, num.charAt(i) - '0');
                 i += 2;
             }
-            else first = num.charAt(i) - '0';
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append(max);
-        sb.append(max);
-        sb.append(max);
-        return max==-1?"":sb.toString();
+        return max == -1 ? "" : String.valueOf((char) (max + '0')).repeat(3);
     }
 }
