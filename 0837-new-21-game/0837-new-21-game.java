9aaa7b81516eb58fd751eb10831f282c3c80937a
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2025-08-17 05:43:34 +0530

    Time: 3 ms (100%), Space: 44 MB (57.69%) - LeetHub

--- 0837-new-21-game/0837-new-21-game.java
+++ 0837-new-21-game/0837-new-21-game.java
@@ -1,24 +1,27 @@
 class Solution {
     public double new21Game(int N, int K, int maxPts) {
-       
-        if (K == 0) return 1.0;
-        if (N >= K - 1 + maxPts) return 1.0;
+        if (K == 0 || N >= K - 1 + maxPts) return 1.0;
 
-        double[] dp = new double[N + 1];
-        Arrays.fill(dp, 0.0);
-
-        double probability = 0.0; 
-        double windowSum = 1.0;
+        double[] dp = new double[maxPts]; 
         dp[0] = 1.0;
+        double windowSum = 1.0, result = 0.0;
+
         for (int i = 1; i <= N; i++) {
-            dp[i] = windowSum / maxPts;
+            double prob = windowSum / maxPts;
+            
+            if (i < K) {
+                windowSum += prob;
+            } else {
+                result += prob;
+            }
 
-            if (i < K) windowSum += dp[i];
-            else probability += dp[i];
+            if (i >= maxPts) {
+                windowSum -= dp[i % maxPts];
+            }
 
-            if (i >= maxPts) windowSum -= dp[i - maxPts];
+            dp[i % maxPts] = prob;
         }
 
-        return probability;
+        return result;
     }
 }
