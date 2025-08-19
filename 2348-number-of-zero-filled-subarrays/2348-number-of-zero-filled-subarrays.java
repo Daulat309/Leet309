05bf7f062ed47d568606d05cb646b83cfc77867a
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2025-08-19 15:30:41 +0530

    Time: 3 ms (94.03%), Space: 62.2 MB (22.98%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -2,8 +2,8 @@ class Solution {
     public long zeroFilledSubarray(int[] nums) {
         long cnt = 0, streak = 0;
         for (int num : nums) {
-            if(num == 0)cnt += ++streak;
-            else streak  = 0;
+            streak = (num == 0) ? streak + 1 : 0;
+            cnt += streak;
         }
         return cnt;
     }
