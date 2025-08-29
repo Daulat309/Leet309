4ce88365b2b5d70028bc9f2911faf45881ea6e75
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2025-08-29 19:26:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.java
@@ -3,11 +3,9 @@ class Solution {
         int n = nums.length;
         if(n==1&&nums[0]!=0) return 1;
         boolean[] ar = new boolean[101];
-        int[] arr = new int[101];
         int k = 0;
         for(int i = 0;i<n;i++){
-            if (ar[nums[i]] == false && ar[i] != 0){
-                arr[k++] = nums[i];
+            if (ar[nums[i]] == false && nums[i] != 0){
                 ar[nums[i]] = true;
             }
         }
