6c658744c6e8acbb1b681eaf11751513b5f0671d
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2025-09-12 23:55:27 +0530

    Time: 3 ms (96.06%), Space: 45.4 MB (82.68%) - LeetHub

---
+++ 0045-vowels-game-in-a-string/0045-vowels-game-in-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        int count = 0;
+        int l = s.length();
+        for(int i = 0;i<l;i++){
+            char ch = s.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') return true;
+        }
+        return false;
+    }
+}
