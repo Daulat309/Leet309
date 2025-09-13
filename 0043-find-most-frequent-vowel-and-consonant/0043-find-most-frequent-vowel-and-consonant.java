5900987657f74d8f78751d65056ebbd7beb25934
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2025-09-13 18:57:08 +0530

    Time: 1 ms (100%), Space: 43.1 MB (18.37%) - LeetHub

--- 0043-find-most-frequent-vowel-and-consonant/0043-find-most-frequent-vowel-and-consonant.java
+++ 0043-find-most-frequent-vowel-and-consonant/0043-find-most-frequent-vowel-and-consonant.java
@@ -3,20 +3,17 @@ class Solution {
         int l = s.length();
         int cmax = 0;
         int vmax = 0;
-        int[] carr = new int[123];
         int[] varr = new int[123];
         for(int i = 0;i<l;i++){
             char ch = s.charAt(i);
             switch (ch){
                 case 'a','e','i','o','u' :
                     varr[(int)ch]++;
-                    if(varr[(int)ch]>vmax) vmax = varr[(int)ch];
+                    vmax = Math.max(vmax,varr[(int)ch]);
                     break;
-                case 'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z' :
-                    carr[(int)ch]++;
-                    if(carr[(int)ch]>cmax) cmax = carr[(int)ch];
-                    break;
-                default : System.out.println();
+                default :
+                    varr[(int)ch]++;
+                    cmax = Math.max(cmax,varr[(int)ch]);
             }
         }
         return cmax + vmax;
