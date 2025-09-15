2c919029117dad166abbab88b54a49a2b73d5945
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2025-09-15 09:25:51 +0530

    Time: 3 ms (48.72%), Space: 41.6 MB (99.07%) - LeetHub

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
@@ -3,14 +3,14 @@ class Solution {
         int p = text.length();
         int q = bL.length();
         int cnt = 0;
-        boolean[] arr = new boolean[123];
+        boolean[] arr = new boolean[26];
         for(int k = 0;k<q;k++){
-            arr[bL.charAt(k)] = true;
+            arr[bL.charAt(k)-'a'] = true;
         }
         for(int k = 0;k<p;k++){
             boolean isit = false;
             while(k<p&&text.charAt(k)!=' '){
-                if(arr[text.charAt(k)]) isit = true;
+                if(arr[text.charAt(k)-'a']) isit = true;
                 k++;
             }
             if(!isit) cnt++;
