699d4a9564c00bae251a2364d977b5b58be89c6c
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2025-09-15 09:05:30 +0530

    Time: 3 ms (48.72%), Space: 42.1 MB (89.56%) - LeetHub

---
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int canBeTypedWords(String text, String bL) {
+        int p = text.length();
+        int q = bL.length();
+        int cnt = 0;
+        boolean[] arr = new boolean[123];
+        for(int k = 0;k<q;k++){
+            arr[bL.charAt(k)] = true;
+        }
+        for(int k = 0;k<p;k++){
+            boolean isit = false;
+            while(k<p&&text.charAt(k)!=' '){
+                if(arr[text.charAt(k)]) isit = true;
+                k++;
+            }
+            if(!isit) cnt++;
+        }
+        return cnt;
+    }
+}
