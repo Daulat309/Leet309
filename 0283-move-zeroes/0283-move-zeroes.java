b4c02894e77d44c012fafb6a8e58fc3731400866
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2025-10-17 09:10:06 +0530

    Time: 2 ms (78.89%), Space: 46.9 MB (7.51%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -4,12 +4,14 @@ class Solution {
         
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                nums[k++] = nums[i];
+                int temp = nums[i];
+                nums[i] = 0;
+                nums[k++] = temp;
             }
         }
         
-        for (int i = k; i < nums.length; i++) {
-            nums[i] = 0;
-        }
+        // for (int i = k; i < nums.length; i++) {
+        //     nums[i] = 0;
+        // }
     }
 }
