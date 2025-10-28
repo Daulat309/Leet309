da984208fbf55de2f760a40a08b81a1ca0eb2edb
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2025-10-28 08:40:57 +0530

    Time: 1782 ms (5.06%), Space: 59.6 MB (75.74%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i = 0;i<n;i++){
             int l = Math.max(i-k,0);
             int r = Math.min(i+k,n-1);
-            for(int j = Math.max(i+1,l);j<i;j++) if(nums[i]==nums[j]) return true;
+            for(int j = l;j<i;j++) if(nums[i]==nums[j]) return true;
             for(int j = i+1;j<=r;j++) if(nums[i]==nums[j]) return true;
         }
         return false;
