d2cc5f3ddf7f769481c21013b611f8e64cc72086
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2025-10-31 21:43:26 +0530

    Time: 1 ms (100%), Space: 45.8 MB (7.71%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -5,7 +5,10 @@ class Solution {
         int[] ar = new int[2];
         int k = 0;
         for(int i = 0;i<n;i++){
-            if(a[nums[i]]==true) ar[k++] = nums[i];
+            if(a[nums[i]]){
+                ar[k++] = nums[i];
+                if(k==2) break;
+            }    
             else a[nums[i]] = true;
         }
         return ar;
