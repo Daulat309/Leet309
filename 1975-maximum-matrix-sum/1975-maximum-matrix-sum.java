3a2111002e50efad1fd6c6b32abcf1e483a0ceeb
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-01-05 11:28:32 +0530

    Time: 7 ms (47.88%), Space: 67.9 MB (68.48%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
@@ -1,23 +1,18 @@
 class Solution {
     public long maxMatrixSum(int[][] m) {
         long sum = 0;
-        // int nmax = -100001;
         int count = 0;
         int abs = 100001;
-        boolean isz = false;
         for(int i = 0;i<m.length;i++){
             for(int j = 0;j<m[i].length;j++){
-                if(m[i][j]==0) isz = true;
                 abs = Math.min(Math.abs(m[i][j]),abs);
                 if(m[i][j]<0){
-                    // nmax = Math.max(m[i][j],nmax);
                     sum += m[i][j]*(-1);
                     count++;
                 }
                 else sum += m[i][j];
             }
         }
-        if(isz) return sum;
         if(count%2==0) return sum;
         return sum - (2*(abs));
     }
