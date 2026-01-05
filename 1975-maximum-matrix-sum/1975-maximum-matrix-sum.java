3abed656046b16116c052d99bab77aedca7890a7
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-01-05 11:08:40 +0530

    Time: 8 ms (19.39%), Space: 68.2 MB (13.94%) - LeetHub

---
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
@@ -0,0 +1,51 @@
+// class Solution {
+//     public long maxMatrixSum(int[][] m) {
+//         long sum = 0;
+//         boolean isit = false;
+//         int lastd = -1000001;
+//         int rcount = 0;
+//         for(int i = 0;i<m.length;i++){
+//             int nmax = -100001;
+//             int count = 0;
+//             for(int j = 0;j<m[i].length;j++){
+//                 if(m[i][j]<0){
+//                     nmax = Math.max(m[i][j],nmax);
+//                     sum += m[i][j]*(-1);
+//                     count++;
+//                 }
+//                 else sum += m[i][j];
+//             }
+//             if(count==m[i].length){
+//                 isit = true;
+//                 if(nmax>lastd) lastd = Math.max(lastd,nmax);
+//                 rcount++;
+//             }
+//         }
+//         if(isit&&rcount%2!=0)return sum-(2*(lastd*-1));
+//         else return sum;
+//     }
+// }
+class Solution {
+    public long maxMatrixSum(int[][] m) {
+        long sum = 0;
+        int nmax = -100001;
+        int count = 0;
+        int abs = 100001;
+        boolean isz = false;
+        for(int i = 0;i<m.length;i++){
+            for(int j = 0;j<m[i].length;j++){
+                if(m[i][j]==0) isz = true;
+                abs = Math.min(Math.abs(m[i][j]),abs);
+                if(m[i][j]<0){
+                    nmax = Math.max(m[i][j],nmax);
+                    sum += m[i][j]*(-1);
+                    count++;
+                }
+                else sum += m[i][j];
+            }
+        }
+        if(isz) return sum;
+        if(count%2==0) return sum;
+        return sum - (2*(abs));
+    }
+}
