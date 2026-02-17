639fb20a9e8c67335a45646bf5c55072f838c6a7
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-02-18 02:19:46 +0530

    Time: 0 ms (100%), Space: 44.9 MB (27.01%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,11 +1,16 @@
 class Solution {
-    static int lowerbound(int[] arr, int element, int lb, int ub){
-        if(lb>ub) return lb;
-        int mid = lb + (ub-lb)/2;
-        if(arr[mid]>=element) return lowerbound(arr,element,lb,mid-1);
-        else return lowerbound(arr,element,mid+1,ub);
+
+    public static int lb(int[] ar, int x){
+        int l = 0;
+        int h = ar.length-1;
+        while(l<=h){
+            int mid = l +(h-l)/2;
+            if(ar[mid]>=x) h = mid-1;
+            else l = mid + 1;
+        }
+        return l;
     }
     public int searchInsert(int[] nums, int target) {
-        return lowerbound(nums,target,0,nums.length-1);
+        return lb(nums,target);
     }
 }
