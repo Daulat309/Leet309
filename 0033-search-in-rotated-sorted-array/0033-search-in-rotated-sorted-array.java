fdb38ff6bca345020048aa48489dc2be4c233d31
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-02-18 20:16:05 +0530

    Time: 0 ms (100%), Space: 43.9 MB (65.68%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,26 +1,21 @@
 class Solution {
-    public static int spos(int[] ar){
-        int i;
-        for(i = ar.length-1;i>0;i--){
-            if(ar[i]<ar[i-1]) break;
-        }
-        return i;
-    }
 
-    public static int bs(int[] ar,int l, int h, int x){
+    public int search(int[] ar, int x) {
+        int l = 0;
+        int h = ar.length - 1;
         while(l<=h){
             int mid = l + (h-l)/2;
             if(ar[mid]==x) return mid;
-            if(ar[mid]>x) h = mid-1;
-            else l = mid+1;
+
+            if(ar[l]<=ar[mid]){
+                if(ar[l]<=x&&x<=ar[mid]) h = mid-1;
+                else l = mid + 1;
+            }
+            else{
+                if(x<=ar[h]&&ar[mid]<x) l = mid + 1;
+                else h = mid-1;
+            }
         }
         return -1;
-    }
-
-    public int search(int[] nums, int target) {
-        int s = spos(nums);
-        if(nums[s]==target) return s;
-        if(nums[nums.length-1]>=target) return bs(nums,s,nums.length-1,target);
-        else return bs(nums,0,s-1,target);
     }
 }
