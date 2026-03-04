312b6d338a56c8cad883dd4f971f28c06d23cc64
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-04 17:01:15 +0530

    Time: 3 ms (74.63%), Space: 48.2 MB (11.07%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -1,37 +1,14 @@
 class Solution {
-    public List<Integer> addToArrayForm(int[] nums, int k) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0;i<nums.length;i++) sb.append(nums[i]);
-        String a1 = sb.toString();
-        String a2 = String.valueOf(k);
-        System.out.println(a1);
-        System.out.println(a2);
-        ArrayList<Integer> list = new ArrayList<>();
-        int i = a1.length()-1;
-        int j = a2.length()-1;
-        int c = 0;
-        while (i >= 0 && j >= 0) {
-            int a = a1.charAt(i)-'0';
-            int b = a2.charAt(j)-'0';
-            list.add((a+b+c)%10);
-            c = (a+b+c)/10;
-            i--;
-            j--;
+        public List<Integer> addToArrayForm(int[] A, int K) {
+        List<Integer> res = new LinkedList<>();
+        for (int i = A.length - 1; i >= 0; --i) {
+            res.add(0, (A[i] + K) % 10);
+            K = (A[i] + K) / 10;
         }
-        while (i >= 0){
-            int a = a1.charAt(i)-'0';
-            list.add((a+c)%10);
-            c = (a+c)/10;
-            i--;
+        while (K > 0) {
+            res.add(0, K % 10);
+            K /= 10;
         }
-        while (j >= 0){
-            int b = a2.charAt(j)-'0';
-            list.add((b+c)%10);
-            c = (b+c)/10;
-            j--;
-        }
-        if(c!=0) list.add(c);
-        Collections.reverse(list);
-        return list;
+        return res;
     }
 }
