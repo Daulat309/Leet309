fff50c5584fc85490279f31e5d84730325614a30
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-04 11:43:19 +0530

    Time: 0 ms (100%), Space: 42.9 MB (85.74%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,9 +1,7 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
         if(sentence.length()<26) return false;
-        boolean[] ar = new boolean[26];
-        for(int i = 0;i<sentence.length();i++) ar[sentence.charAt(i)-'a'] = true;
-        for(int i = 0;i<ar.length;i++) if(ar[i]==false) return false;
+        for(char i = 'a';i<='z';i++) if(sentence.indexOf(i)<0) return false;
         return true;
     }
 }
