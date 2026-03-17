48ae37f5f2b600a90a92a8ff45c07379cee65d3e
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-17 22:32:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,21 +1,30 @@
 class Solution {
-    public static int lb(int ar[], int x){
+    public int[] searchRange(int[] ar, int x) {
+        if(ar.length==0) return new int[] {-1,-1};
+        int[] arr = new int[2];
+        int lbb = lb(ar,x);
+        arr[0] = ar[lbb]==x?lbb:-1;
+        arr[1] = ar[lbb]==x?ub(ar,x)-1:-1;
+        return arr;
+    }
+
+    public static int lb(int[] ar, int x){
         int l = 0;
         int h = ar.length-1;
         while(l<=h){
-            int mid = l +(h-l)/2;
+            int mid = l + (h-l)/2;
             if(ar[mid]>=x) h = mid-1;
-            else l = mid+1;
+            else l = mid + 1;
         }
         return l;
     }
 
-    public static int ub(int ar[], int x){
+    public static int ub(int[] ar, int x){
         int l = 0;
         int h = ar.length-1;
         int ub = ar.length;
         while(l<=h){
-            int mid = l +(h-l)/2;
+            int mid = l + (h-l)/2;
             if(ar[mid]>x){
                 ub = mid;
                 h = mid-1;
@@ -23,18 +32,5 @@ class Solution {
             else l = mid + 1;
         }
         return ub;
-    }
-    public int[] searchRange(int[] nums, int target) {
-        int[] ar = new int[2];
-        int llb = lb(nums,target);
-        if(nums.length==0 ||(llb==nums.length || nums[llb]!=target)){
-            ar[0] = -1;
-            ar[1] = -1;
-            return ar;
-        }
-        int uub = ub(nums,target) - 1;
-        ar[0] = llb;
-        ar[1] = uub;
-        return ar;
     }
 }
