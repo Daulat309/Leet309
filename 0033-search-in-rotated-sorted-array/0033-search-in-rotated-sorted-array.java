e49b23d402804c1f6cd0234b55798fec05691c28
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-18 09:09:48 +0530

    Time: 0 ms (100%), Space: 44 MB (22.39%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,20 +1,27 @@
 class Solution {
-
-    public int search(int[] ar, int x) {
+    public int search(int[] arr, int x) {
         int l = 0;
-        int h = ar.length - 1;
+        int h = arr.length-1;
         while(l<=h){
             int mid = l + (h-l)/2;
-            if(ar[mid]==x) return mid;
-
-            if(ar[l]<=ar[mid]){
-                if(ar[l]<=x&&x<=ar[mid]) h = mid-1;
-                else l = mid + 1;
+            if(arr[mid]<arr[arr.length-1]){
+                if(arr[mid]<=x&&x<=arr[h]) return bs(arr, x, mid, h);
+                else h = mid -1;
             }
             else{
-                if(x<=ar[h]&&ar[mid]<x) l = mid + 1;
-                else h = mid-1;
+                if(arr[l]<=x&&x<=arr[mid]) return bs(arr, x, l, mid);
+                else l = mid + 1;
             }
+        }
+        return -1;
+    }
+
+    public static int bs(int[] arr, int x, int l, int h){
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(arr[mid]==x) return mid;
+            else if(arr[mid]>x) h = mid-1;
+            else l = mid + 1;
         }
         return -1;
     }
