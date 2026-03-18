449e9dd935353a9e7cbeb8a5de40c69d2f861e49
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-18 07:43:13 +0530

    Time: 0 ms (100%), Space: 43.7 MB (98.16%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+        int l  = 0;
+        int h = arr.length-1;
+        while(l<h){
+            int mid = l + (h-l)/2;
+            if(mid+1<arr.length&&arr[mid]>arr[mid+1]){
+                h = mid;
+            }
+            else l = mid + 1;
+        }
+        return h;
+    }
+}
