74cf412c3a878a07418550206db0cf57e8033374
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-18 09:18:43 +0530

    Time: 0 ms (100%), Space: 44 MB (22.39%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -4,24 +4,15 @@ class Solution {
         int h = arr.length-1;
         while(l<=h){
             int mid = l + (h-l)/2;
-            if(arr[mid]<arr[h]){
-                if(arr[mid]<=x&&x<=arr[h]) return bs(arr, x, mid, h);
+            if(arr[mid]==x) return mid;
+            if(arr[mid]<=arr[h]){
+                if(arr[mid]< x && x<=arr[h]) l = mid + 1;
                 else h = mid -1;
             }
             else{
-                if(arr[l]<=x&&x<=arr[mid]) return bs(arr, x, l, mid);
+                if(arr[l]<=x&&x<arr[mid]) h = mid - 1;
                 else l = mid + 1;
             }
-        }
-        return -1;
-    }
-
-    public static int bs(int[] arr, int x, int l, int h){
-        while(l<=h){
-            int mid = l + (h-l)/2;
-            if(arr[mid]==x) return mid;
-            else if(arr[mid]>x) h = mid-1;
-            else l = mid + 1;
         }
         return -1;
     }
