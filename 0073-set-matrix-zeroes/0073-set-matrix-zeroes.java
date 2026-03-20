47ba2b9c168ad34527f677df53298cdd60401850
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-20 13:36:09 +0530

    Time: 1 ms (67.69%), Space: 47.7 MB (18.77%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,16 +1,25 @@
 class Solution {
     public void setZeroes(int[][] ar) {
-        int[][] arr = new int[ar.length][ar[0].length];
+        int[] rw = new int[ar.length];
+        int[] cl = new int[ar[0].length];
         for(int i = 0;i<ar.length;i++){
-            for(int j = 0;j<ar[i].length;j++)if(ar[i][j]==0) arr[i][j] = 1;
+            for(int j = 0;j<ar[i].length;j++)if(ar[i][j]==0){
+                rw[i] = 1;
+                cl[j] = 1;
+            }
         }
-        for(int i = 0;i<ar.length;i++){
-            for(int j = 0;j<ar[i].length;j++)if(arr[i][j]==1) setZ(ar,i,j);
+        for(int i = 0;i<rw.length;i++){
+            if(rw[i]==1) setrZ(ar,i);
+        }
+        for(int i = 0;i<cl.length;i++){
+            if(cl[i]==1) setcZ(ar,i);
         }
     }
 
-    public void setZ(int[][] ar, int r, int c){
-        for(int i = 0;i<ar.length;i++) ar[i][c] = 0;
+    public void setrZ(int[][] ar, int r){
         for(int j = 0;j<ar[0].length;j++) ar[r][j] = 0;
+    }
+    public void setcZ(int[][] ar,int c){
+        for(int i = 0;i<ar.length;i++) ar[i][c] = 0;
     }
 }
