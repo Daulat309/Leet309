7dfc1ac1efb4fb28d1da995aaedbb97fab68b2b3
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-20 15:27:27 +0530

    Time: 3 ms (23.6%), Space: 48 MB (35.8%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -4,6 +4,7 @@ class Solution {
         for(int i = 0;i<nums.length-1;i++){
             cp = Math.max(cp,nums[i] + i);
             if(cp<i+1) return false;
+            if(nums.length-1-nums[i]-i<=0) return true;
         }
         return true;
     }
