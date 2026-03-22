9574b69156b499ce67eb5e7bbf4cf7d20886cc3d
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-22 10:40:36 +0530

    Time: 4 ms (88.83%), Space: 121.5 MB (68.88%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,9 +1,12 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        Arrays.sort(nums1);
+        int min = Integer.MAX_VALUE;
         int odd = 0;
-        for(int i = 0;i<nums1.length;i++) if((nums1[i] & 1)==1) odd++;
-        if((nums1[0]&1)==1||(odd==0)||nums1.length==odd) return true;
+        for(int i = 0;i<nums1.length;i++){
+            if((nums1[i] & 1)==1) odd++;
+            min = Math.min(nums1[i],min);
+        }
+        if((min&1)==1||odd==0) return true;
         return false;
     }
 }
