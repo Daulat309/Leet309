9940c6c0ce4ab33bc94e16f050e8b79d587d22fa
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-24 07:34:53 +0530

    Time: 0 ms (100%), Space: 42.2 MB (51.04%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPerfectSquare(int x) {
+        if(x==0) return true;
+        int l = 1, h = x, r = 0;
+        while(l<=h){
+            int m = l + (h-l)/2;
+            if(m*m==x) return true;
+            if(m<x/m) l = m + 1;
+            else h = m-1;
+        }
+        return false;
+    }
+}
