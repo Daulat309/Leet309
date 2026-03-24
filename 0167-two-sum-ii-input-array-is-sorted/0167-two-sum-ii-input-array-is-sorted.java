6746a2fb80f725bd9ffd33ede4591c1cd7066130
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-24 08:16:33 +0530

    Time: 10 ms (8.33%), Space: 55.9 MB (5.48%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] a, int x) {
+        for(int i = 0;i<a.length;i++){
+            int l = i + 1, h = a.length-1;
+            while(l<=h){
+                int m = l + (h-l)/2;
+                if(a[m]+a[i]==x){
+                    System.out.println(i+" "+m+" "+a[m]+" "+ (x-a[i]));
+                    return new int[]{i+1,m+1};
+                }
+                if(a[m]+a[i]>x) h = m -1;
+                else l = m + 1;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
