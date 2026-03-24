fbe771c8381c18760a71803b0ce5e1cb8b1cc1b6
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-24 07:29:41 +0530

    Time: 1 ms (97.49%), Space: 42.4 MB (86.54%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,22 +1,15 @@
 class Solution {
     public int mySqrt(int x) {
         if(x==0) return 0;
-        int a = (int) sqrt(x);
-        return a;
-    }
-
-    public double sqrt(double x){
-        double l = Math.min(1,x);
-        double h = Math.max(1,x);
-        while(100*l*l<x) l *= 10;
-        while(0.01*h*h>x) h *= 0.1;
-        double m = 1;
-        for(int i = 0;i<100;i++){
-            m = l + (h-l)/2;
-            if(m*m==x) return m;
-            if(m*m>x) h = m-1;
-            else l = m + 1;
+        int l = Math.min(1, x),h = Math.max(1, x), r = 0;
+        while(l<=h){
+            int m = l + (h-l)/2;
+            if(m<=x/m){
+                r = m;
+                l = m+1;
+            }
+            else h = m-1;
         }
-        return m;
+        return r;
     }
 }
