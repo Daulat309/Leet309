d34eb2075fa74fa25c7cbe42f9885628f04bfdd7
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-27 09:38:00 +0530

    Time: 12 ms (3.2%), Space: 52.6 MB (6.67%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean areSimilar(int[][] a, int k) {
+        int odd = 0, even = 0, ev = 0, od= 0;
+        k = k % a[0].length;
+        for(int i = 0;i<a.length;i++){
+            if((i & 1)!=1){for(int j = 0;j<a[i].length;j++){
+                ev++;
+                if(j-k<0){
+                    if(a[i][j]==a[i][a[0].length-(k-j)]) even++;
+                }
+                else {
+                    if(a[i][j]==a[i][j-k]) even++;
+                }
+            }
+            }
+            else for(int j = 0;j<a[0].length;j++){
+                od++;
+                if(j+k>a[0].length-1){
+                    if(a[i][j]==a[i][k-(a[0].length-1-j)-1]) odd++;
+                }
+                else{
+                    if(a[i][j]==a[i][j+k]) odd++;
+                }
+            }
+        }
+        System.out.println("Even : "+even+" Ev : "+ev+" Odd : "+odd+" Od : "+od);
+        return od==odd&&ev==even;
+    }
+}
