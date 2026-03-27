ad1effad0184da0d08ceec1dc2511704f7943fea
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-27 09:42:52 +0530

    Time: 1 ms (99.47%), Space: 47 MB (41.6%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -1,20 +1,18 @@
 class Solution {
     public boolean areSimilar(int[][] a, int k) {
-        int odd = 0, even = 0, ev = 0, od= 0;
+        int odd = 0, even = 0;
         k = k % a[0].length;
         for(int i = 0;i<a.length;i++){
             if((i & 1)!=1){for(int j = 0;j<a[i].length;j++){
-                ev++;
                 if(j-k<0){
                     if(a[i][j]==a[i][a[0].length-(k-j)]) even++;
                 }
-                else {
+                else{
                     if(a[i][j]==a[i][j-k]) even++;
                 }
             }
             }
             else for(int j = 0;j<a[0].length;j++){
-                od++;
                 if(j+k>a[0].length-1){
                     if(a[i][j]==a[i][k-(a[0].length-1-j)-1]) odd++;
                 }
@@ -23,7 +21,6 @@ class Solution {
                 }
             }
         }
-        System.out.println("Even : "+even+" Ev : "+ev+" Odd : "+odd+" Od : "+od);
-        return od==odd&&ev==even;
+        return (a.length/2)*a[0].length==odd&&(a.length/2+a.length%2)*a[0].length==even;
     }
 }
