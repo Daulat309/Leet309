a6de1dffa80d2bdf634210b74bb413b0f910bbd7
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-30 07:46:48 +0530

    Time: 5 ms (98.68%), Space: 48 MB (65.79%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,19 +1,13 @@
 class Solution {
     public boolean checkStrings(String s1, String s2) {
-        int[] ef1 = new int[26];
-        int[] of1 = new int[26];
-        int[] ef2 = new int[26];
-        int[] of2 = new int[26];
-        for(int i = 0;i<s1.length();i += 2){
-            ef1[s1.charAt(i)-'a']++;
-            ef2[s2.charAt(i)-'a']++;
+        int[] f = new int[52];
+        for(int i = 0;i<s1.length();i++){
+            int of = (i&1)*26;
+            f[s1.charAt(i)-'a' + of]++;
+            f[s2.charAt(i)-'a' + of]--;
         }
-        for(int i = 1;i<s1.length();i += 2){
-            of1[s1.charAt(i)-'a']++;
-            of2[s2.charAt(i)-'a']++;
-        }
-        for(int i = 0;i<26;i++){
-            if(ef1[i]!=ef2[i] || of1[i]!=of2[i]) return false;
+        for(int i = 0;i<52;i++){
+            if(f[i]!=0) return false;
         }
         return true;
     }
