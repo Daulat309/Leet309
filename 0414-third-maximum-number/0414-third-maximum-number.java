f3bf4791109311e6c5a9ded04fa955d252afc4fd
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-03-31 18:18:51 +0530

    Time: 13 ms (5.39%), Space: 46.5 MB (13.88%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,13 +1,11 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        if(nums.length<2) return nums[0];
-        Arrays.sort(nums);
         HashSet<Integer> set = new HashSet<>();
-        for(int i : nums) set.add(i);
-        nums = set.stream().mapToInt(Integer::intValue).toArray();
-        System.out.println(nums.length);
-        if(nums.length<2) return nums[0];
-        if(nums.length<3) return nums[1];
-        return nums[nums.length-1-2];
+        for (int i : nums) set.add(i);
+        int[] arr = set.stream().mapToInt(Integer::intValue).toArray();
+        Arrays.sort(arr);
+        int n = arr.length;
+        if (n < 3) return arr[n - 1];
+        return arr[n - 3];
     }
 }
