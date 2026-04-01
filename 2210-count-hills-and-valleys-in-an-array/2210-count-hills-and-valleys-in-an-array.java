8b96dcdf24f335bf26463b16d91b8f0a36f52596
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-01 14:10:12 +0530

    Time: 0 ms (100%), Space: 43.4 MB (47.96%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
@@ -1,34 +1,20 @@
 class Solution {
-    // public int l(int[] arr,int j,int k){
-    //     for(int i = j-1;i>=0;i--){
-    //         if(arr[i]!=k) return arr[i];
-    //     }
-    //     return k;
-    // }
-    // public int r(int[] arr,int j,int k){
-    //     for(int i = j+1;i<arr.length;i++){
-    //         if(arr[i]!=k) return arr[i];
-    //     }
-    //     return k;
-    // }
-    public int countHillValley(int[] arr) {
-        int count = 0;
-        for(int i = 1;i<arr.length-1;i++){
-            if(arr[i] == arr[i-1]) continue;
-            int prev = i-1;
-            int next = i+1;
-            while(prev >= 0 && arr[i] == arr[prev]) {
-                prev--;
+    public int countHillValley(int[] a) {
+        int cnt = 0;
+        int l = 0,k = l + 1, h = l + 2;
+        while(h<a.length){
+            if(a[l]>a[k]&&a[k]<a[h]||a[l]<a[k]&&a[k]>a[h]){
+                cnt++;
+                k = h;
+                h++;
+                l = k-1;
             }
-            while(next < arr.length && arr[i] == arr[next]) {
-                next++;
-            }
-            if(prev >= 0 && next < arr.length){
-                if((arr[i]<arr[prev]&&arr[i]<arr[next])||(arr[i]>arr[prev]&&arr[i]>arr[next])){
-                    count++;
-                }
+            else if(a[k]==a[h]) h++;
+            else {
+                l = k;
+                k = h;
             }
         }
-        return count;
+        return cnt;
     }
 }
