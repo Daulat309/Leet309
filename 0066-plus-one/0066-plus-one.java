5b661989d8ce9133e7a0b201a8dcb9cb3d026805
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-02 08:35:56 +0530

    Time: 0 ms (100%), Space: 43.3 MB (85.13%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,14 +1,23 @@
 class Solution {
     public int[] plusOne(int[] d) {
-        ArrayList<Integer> list = new ArrayList<>();
         int car = 1;
         for(int i = d.length-1;i>=0;i--){
-            list.add(0,((d[i]+car)%10));
-            car = (d[i]+car)/10;
+            int t = d[i];
+            d[i] = (d[i]+car)%10;
+            car = (t+car)/10;
         }
-        if(car!=0)list.add(0,car);
-        int[] ar = new int[list.size()];
-        for(int i = 0;i<ar.length;i++) ar[i] = list.get(i);
-        return ar;
+        ArrayList<Integer> list = new ArrayList<>();
+        if(car!=0){
+            while(car>0){
+                list.add(car%10);
+                car /= 10;
+            }
+            int[] a = new int[list.size()+d.length];
+            int i = 0;
+            for(i = 0;i<list.size();i++) a[i]=list.get(list.size()-1-i);
+            for(i = i;i<a.length;i++) a[i]=d[i-list.size()];
+            return a;
+        }
+        return d;
     }
 }
