05a5c91763cfebd9c6c3ab78ffdd1861557ba7db
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-02 08:12:09 +0530

    Time: 3 ms (1.16%), Space: 43.1 MB (21.32%) - LeetHub

---
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minCostToMoveChips(int[] p) {
+        if(p.length<2) return 0;
+        int e = 0, o = 0;
+        for(int i = 0;i<p.length;i++){
+            if((p[i]&1)==1) o++;
+            else e++;
+        }
+        System.out.println(e+" "+o);
+        return Math.min(e,o);
+    }
+}
