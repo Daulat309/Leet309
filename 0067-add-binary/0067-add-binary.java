a4536c41ed57d8395ce126bede910e21ed721213
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-07 10:39:36 +0530

    Time: 2 ms (51.12%), Space: 43.5 MB (69.19%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,46 +1,20 @@
 class Solution {
     public String addBinary(String a1, String a2) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int c = 0;
-        int i = a1.length()-1, j = a2.length()-1;
-        while(i>=0&&j>=0){
-            int s = a1.charAt(i)-'0' + a2.charAt(j)-'0' + c;
-            if(s==2||s==3) c = 1;
-            else c = 0;
-            if(s==3||s==1) s = 1;
-            else s = 0;
-            list.add(0,s);
-            System.out.println(list);
-            i--;
-            j--;
-        }
-        while(i>=0){
-            int s = a1.charAt(i)-'0' + c;
-            if(s==2) c = 1;
-            else c = 0;
-            if(s==1) s = 1;
-            else s = 0;
-            list.add(0,s);
-            System.out.println(list);
-            i--;
-        }
-        while(j>=0){
-            int s = a2.charAt(j)-'0' + c;
-            if(s==2) c = 1;
-            else c = 0;
-            if(s==1) s = 1;
-            else s = 0;
-            list.add(0,s);
-            System.out.println(list);
-            j--;
-        }
-        if(c!=0) list.add(0,c);
         StringBuilder sb = new StringBuilder();
-        i = 0;
-        while(i<list.size()){
-            sb.append(list.get(i));
-            i++;
+        
+        int i = a1.length() - 1;
+        int j = a2.length() - 1;
+        int c = 0;
+        
+        while (i >= 0 || j >= 0 || c == 1) {
+            int sum = c;
+            
+            if (i >= 0) sum += a1.charAt(i--) - '0';
+            if (j >= 0) sum += a2.charAt(j--) - '0';
+            
+            sb.append(sum % 2); 
+            c = sum / 2;         
         }
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
