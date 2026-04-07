dd212179e2572569a5cc07fa76293c0f80f4bb4c
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-07 10:31:28 +0530

    Time: 77 ms (9.01%), Space: 47.3 MB (5.3%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,65 +1,46 @@
 class Solution {
     public String addBinary(String a1, String a2) {
-        StringBuilder sb = new StringBuilder();
-        int i = a1.length()-1;
-        int j = a2.length()-1;
+        ArrayList<Integer> list = new ArrayList<>();
         int c = 0;
-        while (i >= 0 && j >= 0) {
-            int a = a1.charAt(i)-'0';
-            int b = a2.charAt(j)-'0';
-            if(a+b+c==3){
-                sb.append(1);
-                c = 1;
-            }
-            else if(a+b+c==2){
-                sb.append(0);
-                c = 1;
-            }
-            else if(a+b+c==1){
-                sb.append(1);
-                c = 0;
-            }
-            else{
-                sb.append(0);
-                c = 0;
-            }
+        int i = a1.length()-1, j = a2.length()-1;
+        while(i>=0&&j>=0){
+            int s = a1.charAt(i)-'0' + a2.charAt(j)-'0' + c;
+            if(s==2||s==3) c = 1;
+            else c = 0;
+            if(s==3||s==1) s = 1;
+            else s = 0;
+            list.add(0,s);
+            System.out.println(list);
             i--;
             j--;
         }
-        while (i >= 0){
-            int a = a1.charAt(i)-'0';
-            if(a+c==2){
-                sb.append(0);
-                c = 1;
-            }
-            else if(a+c==1){
-                sb.append(1);
-                c = 0;
-            }
-            else{
-                sb.append(0);
-                c = 0;
-            }
+        while(i>=0){
+            int s = a1.charAt(i)-'0' + c;
+            if(s==2) c = 1;
+            else c = 0;
+            if(s==1) s = 1;
+            else s = 0;
+            list.add(0,s);
+            System.out.println(list);
             i--;
         }
-        while (j >= 0){
-            int b = a2.charAt(j)-'0';
-            if(b+c==2){
-                sb.append(0);
-                c = 1;
-            }
-            else if(b+c==1){
-                sb.append(1);
-                c = 0;
-            }
-            else{
-                sb.append(0);
-                c = 0;
-            }
+        while(j>=0){
+            int s = a2.charAt(j)-'0' + c;
+            if(s==2) c = 1;
+            else c = 0;
+            if(s==1) s = 1;
+            else s = 0;
+            list.add(0,s);
+            System.out.println(list);
             j--;
         }
-        if(c!=0) sb.append(1);
-        sb.reverse();
+        if(c!=0) list.add(0,c);
+        StringBuilder sb = new StringBuilder();
+        i = 0;
+        while(i<list.size()){
+            sb.append(list.get(i));
+            i++;
+        }
         return sb.toString();
     }
 }
