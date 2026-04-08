08437e4f899bb8be17e10c9bcc720d1260f1b583
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-08 10:06:57 +0530

    Time: 68 ms (8%), Space: 47.9 MB (52%) - LeetHub

--- 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.java
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.java
@@ -1,28 +1,14 @@
 class Solution {
-    String SS = "Hare Krishna!";
-
-    private static final int MOD = 1_000_000_007;
-    
-    private void applyQuery(int[] nums, int l, int r, int k, int v){
-        for(int i = l;i<=r; i += k){
-            long prod = (long)nums[i]*v;
-            nums[i] = (int)(prod%MOD);
+    public int xorAfterQueries(int[] n, int[][] q) {
+        int mod = 1_000_000_007;
+        for(int[] a:q){
+            for(int i = a[0];i<=a[1];i += a[2]){
+                long p = (long)(n[i])*a[3];
+                n[i] =(int) (p%mod);
+            }
         }
-    }
-    
-    public int xorAfterQueries(int[] nums, int[][] queries) {
-        for(int[] q : queries){
-            int l = q[0];
-            int r = q[1];
-            int k = q[2];
-            int v = q[3];
-
-            applyQuery(nums, l, r, k, v);
-        }
-        int xor = 0;
-        for(int n : nums){
-            xor ^= n;
-        }
-        return xor;
+        int s = 0;
+        for(int i : n) s ^= i;
+        return s;
     }
 }
