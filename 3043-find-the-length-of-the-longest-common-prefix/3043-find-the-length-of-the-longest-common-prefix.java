d981c969f9ef63d2982aea5e86aebbfe5d0a1088
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-12 18:26:55 +0530

    Time: 40 ms (92.89%), Space: 81 MB (67.33%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -1,79 +1,76 @@
-class Solution {
-    public int longestCommonPrefix(int[] a, int[] b) {
-        HashSet<String> set = new HashSet<>();
-        for(int i : a){
-            String s = String.valueOf(i);
-            String p = "";
-            for(char ch : s.toCharArray()){
-                p += ch;
-                set.add(p);
-            }
-        }
-        int ml = 0;
-        for(int i : b){
-            String s = String.valueOf(i);
-            String p = "";
-            for(char ch : s.toCharArray()){
-                p += ch;
-                if(set.contains(p)) ml = Math.max(ml,p.length());
-            }
-        }
-        return ml;
+// class Solution {
+//     public int longestCommonPrefix(int[] a, int[] b) {
+//         HashSet<String> set = new HashSet<>();
+//         for(int i : a){
+//             String s = String.valueOf(i);
+//             String p = "";
+//             for(char ch : s.toCharArray()){
+//                 p += ch;
+//                 set.add(p);
+//             }
+//         }
+//         int ml = 0;
+//         for(int i : b){
+//             String s = String.valueOf(i);
+//             String p = "";
+//             for(char ch : s.toCharArray()){
+//                 p += ch;
+//                 if(set.contains(p)) ml = Math.max(ml,p.length());
+//             }
+//         }
+//         return ml;
+//     }
+// }
+
+
+
+
+
+class Node{
+    Node[] child;
+
+    public Node(){
+        child = new Node[10];
     }
 }
 
+class Solution {
+    Node root;
 
+    public Solution(){
+        root = new Node();
+    }
 
+    public int longestCommonPrefix(int[] a, int[] b) {
+        Solution obj = new Solution();
+        int ml = 0;
+        for(int i : a)inserta(Integer.toString(i));
+        for(int i : b) ml = Math.max(ml,search(Integer.toString(i)));
+        return ml;
+    }
 
+    public void inserta(String w){
+        Node current = root;
+        for(char ch : w.toCharArray()){
+            int idx = ch - '0';
+            if(current.child[idx]==null){
+                Node node = new Node();
+                current.child[idx] = node;
+            }
+            current = current.child[idx];
+        }
+    }
 
-// class Node{
-//     Node[] child;
-//     int cpa;
-//     int cpb;
+    public int search(String w){
+        Node current = root;
+        int i = 0;
+        for(char ch : w.toCharArray()){
+            int idx = ch - '0';
+            if(current.child[idx]==null) return i;
+            current = current.child[idx];
+            i++;
+        }
+        return i;
+    }
 
-//     public Node(){
-//         child = new Node[10];
-//         cpa = 0;
-//         cpb = 0;
-//     }
-// }
-
-// class Solution {
-//     Node root;
-
-//     public Solution(){
-//         root = new Node();
-//     }
-
-//     public int longestCommonPrefix(int[] a, int[] b) {
-//         Solution obj = new Solution();
-//         for(int i : a)inserta(Integer.toString(i));
-//         for(int i : b)insertb(Integer.toString(i));
-//     }
-
-//     public void inserta(String w){
-//         Node current = root;
-//         for(char ch : w.toCharArray()){
-//             int idx = ch - '0';
-//             if(current.child[idx]==null){
-//                 Node node = new Node();
-//                 current.child[idx] = node;
-//             }
-//             current = current.child[idx];
-//             current.cpa++;
-//         }
-//     }
-
-//     public void insertb(String w){
-//         Node current = root;
-//         for(char ch : w.toCharArray()){
-//             int idx = ch - '0';
-//             if(current.child[idx]==null){
-//                 Node node = new Node();
-//                 current.child[idx] = node;
-//             }
-//             current = current.child[idx];
-//             current.cpb++;
-//         }
-//     }
-// }
+}
