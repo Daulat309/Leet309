0e20158808f035086c83585d35ec290b87fab128
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-13 08:23:21 +0530

    Time: 0 ms (100%), Space: 44.8 MB (84.16%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
@@ -1,20 +1,9 @@
 class Solution {
     public int getMinDistance(int[] nums, int target, int start) {
-        int l = start, r = start+1;
-        while(l>=0&&r<nums.length){
-            if(nums[l]==target)return start - l;
-            else if(nums[r]==target)return r-start;
-            l--;
-            r++;
+        for (int d = 0; d < nums.length; d++) {
+            if (start - d >= 0 && nums[start - d] == target) return d;
+            if (start + d < nums.length && nums[start + d] == target) return d;
         }
-        while(l>=0){
-            if(nums[l]==target)return start - l;
-            l--;
-        }
-        while(r<nums.length){
-            if(nums[r]==target)return r-start;
-            r++;
-        }
-        return start;
+        return 0;
     }
 }
