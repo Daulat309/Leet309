241074cde2a545cdb10749270c8a082f5cbbe83a
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-13 17:42:15 +0530

    Time: 4 ms (8.34%), Space: 116.1 MB (68.87%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -1,13 +1,9 @@
 class Solution {
-    public int[] findPeakGrid(int[][] mat) {
-        for(int i = 0;i<mat.length;i++){
-            for(int j = 0;j<mat[i].length;j++){
-                int cur = mat[i][j];
-                int l = j==0?-1:mat[i][j-1];
-                int r = j==mat[i].length-1?-1:mat[i][j+1];
-                int u = i==0?-1:mat[i-1][j];
-                int d = i==mat.length-1?-1:mat[i+1][j];
-                if(cur>l&&cur>r&&cur>u&&cur>d) return new int[]{i,j};
+    public int[] findPeakGrid(int[][] a) {
+        int m = a.length, n = a[0].length;
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if((i-1<0||a[i-1][j]<a[i][j])&&(i+1>=a.length||a[i+1][j]<a[i][j])&&(j-1<0||a[i][j-1]<a[i][j])&&(j+1>=n||a[i][j+1]<a[i][j])) return new int[]{i,j};
             }
         }
         return new int[]{0,0};
