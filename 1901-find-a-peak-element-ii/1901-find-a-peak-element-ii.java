a4b08188f2cfc2abcfe7ba191b2dca7e19a0c240
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-13 20:07:36 +0530

    Time: 0 ms (100%), Space: 116.1 MB (68.87%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -1,11 +1,32 @@
 class Solution {
-    public int[] findPeakGrid(int[][] a) {
-        int m = a.length, n = a[0].length;
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if((i-1<0||a[i-1][j]<a[i][j])&&(i+1>=a.length||a[i+1][j]<a[i][j])&&(j-1<0||a[i][j-1]<a[i][j])&&(j+1>=n||a[i][j+1]<a[i][j])) return new int[]{i,j};
-            }
+    public int[] findPeakGrid(int[][] mat) {
+        int l = 0, h = mat[0].length-1;
+        while(l<=h){
+            int m = l + (h-l)/2;
+            int mr = findMax(mat,m);
+            if(m==0&&(m==mat[0].length-1||mat[mr][m]>mat[mr][m+1])) return new int[]{mr,m};
+            if(m==mat[0].length-1&&(m==0||mat[mr][m]>mat[mr][m-1])) return new int[]{mr,m};
+            if((m-1>=0&&mat[mr][m]>mat[mr][m-1])&&((m+1<mat[0].length&&mat[mr][m]>mat[mr][m+1]))) return new int[]{mr,m};
+            if((m-1>=0&&mat[mr][m]<mat[mr][m-1])) h = m - 1;
+            else l = m + 1;
         }
         return new int[]{0,0};
+    }
+
+    public int findMax(int[][] mat, int mid){
+        int l = 0, r = mat.length-1, mx = Integer.MIN_VALUE,mr = 0;
+        while(l<=r){
+            if(mat[l][mid]>mx){
+                mx = mat[l][mid];
+                mr = l;
+            }
+            if(mat[r][mid]>mx){
+                mx = mat[r][mid];
+                mr = r;
+            }
+            l++;
+            r--;
+        }
+        return mr;
     }
 }
