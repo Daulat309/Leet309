fba877dc10382d0316875a1772ade5bd2bfef181
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-13 09:56:44 +0530

    Time: 170 ms (50%), Space: 93.4 MB (0%) - LeetHub

---
+++ 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int cnt = 0;
+        if(nums.length<2){
+            while(!isPrime(nums[0])){
+                nums[0]++;
+                cnt++;
+            }
+            return cnt;
+        }
+        for(int i = 0;i<nums.length;i += 2){
+            if(!isPrime(nums[i])){
+                nums[i]++;
+                cnt++;
+                i -= 2;
+            }
+        }
+        for(int i = 1;i<nums.length;i += 2){
+            if(isPrime(nums[i])){
+                nums[i]++;
+                cnt++;
+                i -= 2;
+            }
+        }
+        return cnt;
+    }
+
+    public boolean isPrime(int n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
+    }
+}
