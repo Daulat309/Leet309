444e7f994360e7b14c5f844c4db222918c7302e5
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-15 07:48:22 +0530

    Time: 0 ms (100%), Space: 46.3 MB (91.56%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
@@ -1,6 +1,5 @@
 class Solution {
     public int closestTarget(String[] w, String t, int s) {
-        //int l = Math.max(0,s-1), h = Math.min(s+1,w.length-1);
         if(w[s].equals(t)) return 0;
         int n = w.length;
         for(int i = 0;i<w.length;i++){
