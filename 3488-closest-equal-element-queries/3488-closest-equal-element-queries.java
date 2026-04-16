f35cff49689da3e37f2a96c210ec1b689f7512e8
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-16 12:05:09 +0530

    Time: 697 ms (5.05%), Space: 179.5 MB (92.93%) - LeetHub

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.java
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.java
@@ -1,27 +1,32 @@
 class Solution {
     public List<Integer> solveQueries(int[] n, int[] q) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int j = 0;j<q.length;j++){
-            int s = q[j];
-            int t = n[s];
-            boolean found = true;
-            for(int i = 1;i<(n.length)/2+1;i++){
-                if(n[(s+i)%n.length]==t){
-                    list.add(i);
-                    found = false;
-                    break;
-                }
-                if(n[(n.length+s-i)%n.length]==t){
-                    list.add(i);
-                    found = false;
-                    break;
-                }
-            }
-            if(found){
-                System.out.println(-1);
-                list.add(-1);
-            }
+        HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
+        for(int i = 0;i<n.length;i++){
+            map.putIfAbsent(n[i],new ArrayList<Integer>());
+            map.get(n[i]).add(i);
         }
-        return list;
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int s : q){
+            int v = n[s];
+            ArrayList<Integer> list = map.get(v);
+
+            if(list.size()==1){
+                res.add(-1);
+                continue;
+            }
+
+            int idx = Collections.binarySearch(list,s);
+            int prev = list.get((idx-1+list.size())%list.size());
+            int next = list.get((idx+1)%list.size());
+
+            int d1 = Math.abs(s-prev);
+            int d2 = Math.abs(s-next);
+            System.out.println(d1+"  "+d2);
+            d1 = Math.min(n.length-d1,d1);
+            d2 = Math.min(n.length-d2,d2);
+            
+            res.add(Math.min(d1,d2));
+        }
+        return res;
     }
 }
