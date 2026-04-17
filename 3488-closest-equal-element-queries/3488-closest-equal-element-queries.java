c2460494b1793ba0488e01f419c43bc89e9a15c5
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-17 05:47:22 +0530

    Time: 148 ms (16.33%), Space: 188.2 MB (40.82%) - LeetHub

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.java
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.java
@@ -19,9 +19,9 @@ class Solution {
             int prev = list.get((idx-1+list.size())%list.size());
             int next = list.get((idx+1)%list.size());
 
-            int d1 = Math.abs(s-prev);
+            int d1 = Math.abs(s-prev);//abs diff
             int d2 = Math.abs(s-next);
-            d1 = Math.min(n.length-d1,d1);
+            d1 = Math.min(n.length-d1,d1);//circular way
             d2 = Math.min(n.length-d2,d2);
             
             res.add(Math.min(d1,d2));
