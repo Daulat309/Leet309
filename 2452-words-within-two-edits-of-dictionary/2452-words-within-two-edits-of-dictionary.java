269fdf2caba7581638a35c23749bbbc0d41d34ea
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-22 07:46:51 +0530

    Time: 6 ms (72.79%), Space: 47.2 MB (19.85%) - LeetHub

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
@@ -0,0 +1,43 @@
+class Node{
+    Node[] child = new Node[26];
+    boolean eow = false;
+}
+
+class Solution {
+    Node root = new Node();
+    public List<String> twoEditWords(String[] q, String[] d){
+        ArrayList<String> list = new ArrayList<>();
+        for(String S : d){
+            insert(S);
+        }
+        for(String S : q){
+            if(dfs(S,0,0,root)) list.add(S);
+        }
+        return list;
+    }
+
+    public void insert(String q){
+        Node current = root;
+        for(char c : q.toCharArray()){
+            int idx = c-'a';
+            if(current.child[idx]==null){
+                current.child[idx] = new Node();
+            }
+            current = current.child[idx];
+        }
+        current.eow = true;
+    }
+
+    public boolean dfs(String s,int idx,int er,Node node){
+        if(er>2) return false;
+        if(idx==s.length()) return node.eow;
+        for(int i = 0;i<26;i++){
+            
+            if(node.child[i]!=null){
+                int ner = er + (i!=(s.charAt(idx)-'a')?1:0);
+                if(dfs(s,idx+1,ner,node.child[i])) return true;;
+            }
+        }
+        return false;
+    }
+}
