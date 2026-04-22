bf7a4aafcc6daad7aab1af32768cbd4a2aa9ae4a
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-04-22 10:15:15 +0530

    Time: 4 ms (59.88%), Space: 46.4 MB (67.41%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,15 +1,23 @@
 class Solution {
+
     public int romanToInt(String s) {
+
         int sm = 0;
+
         for(int i = 0;i<s.length();i++){
+
             int cur = val(s.charAt(i));
+
             if(i<s.length()-1&&cur<val(s.charAt(i+1))) sm-= cur;
             else sm += cur;
+
         }
+
         return sm;
     }
 
-    public int val(char c){
+    private int val(char c){
+
         switch(c){
             case 'I' : return 1;
             case 'V' : return 5;
@@ -18,6 +26,7 @@ class Solution {
             case 'C' : return 100;
             case 'D' : return 500;
             default : return 1000;
+
         }
     }
 }
