055e72172bd5d9433149382932d61db6cae95f02
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-02 09:54:01 +0530

    Time: 34 ms (60.46%), Space: 59.3 MB (45.68%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,30 +2,27 @@ class Solution {
     public List<List<Integer>> threeSum(int[] n) {
         List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(n);
+
         for(int i = 0;i<n.length-2;i++){
-            for(int j = i + 1;j<n.length-1;j++){
-                int s = n[i]+n[j];
-                int x = 0 - s;
-                int start = j+1;
-                int end = n.length-1;
-                int bs = bS(n,start,end,x);
-                if(bs!=1000000){
-                    list.add(Arrays.asList(n[i], n[j], bs));
+            if(i > 0 && n[i] == n[i - 1]) continue;
+            int x = -n[i];
+            int l = i + 1, h = n.length-1;
+            while(l<h){
+                int sm = n[l]+n[h];
+                if(sm==x){
+                    list.add(Arrays.asList(n[i],n[l],n[h]));
+                    l++;
+                    h--;
+                    while(l<h&&n[l]==n[l-1]) l++;
+                    while(l<h&&n[h]==n[h+1]) h--;
                 }
-                while(j+1<n.length&&n[j]==n[j+1]) j++;
+                else{
+                    if(sm>x) h--;
+                    else l++;
+                }
+
             }
-            while(i+1<n.length&&n[i]==n[i+1]) i++;
         }
         return list;
-    }
-
-    public static int bS(int[] a, int s, int e, int x){
-        while(s<=e){
-            int md = s+(e-s)/2;
-            if(a[md]==x) return x;
-            if(a[md]<x) s = md+1;
-            else e = md-1;
-        }
-        return 1000000;
     }
 }
