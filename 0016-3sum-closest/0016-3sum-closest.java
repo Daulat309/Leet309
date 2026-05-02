5e1262eb8d8629404a6df4d731bae01ae268a982
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-02 10:30:16 +0530

    Time: 17 ms (78.22%), Space: 45.6 MB (30.74%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int threeSumClosest(int[] n, int x) {
+        Arrays.sort(n);
+        int s = n[0]+n[1]+n[2];
+        int diff = 2001;
+        for(int i = 0;i<n.length-2;i++){
+            if(i > 0 && n[i] == n[i-1]) continue;
+            int l = i + 1, h = n.length-1;
+            while(l<h){
+                int sm = n[i]+n[l]+n[h];
+                if(Math.abs(s-x)>Math.abs(sm-x)){
+                    s = sm;
+                }
+                if(sm>x) h--;
+                else if(sm<x) l++;
+                else return sm;
+            }
+        }
+        return s;
+    }
+}
