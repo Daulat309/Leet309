6a31b1a2d98c507cdf74623914155522d3d88849
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-03 12:07:08 +0530

    Time: 7 ms (96.38%), Space: 73.1 MB (8.37%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,28 +1,41 @@
 class Solution {
-    public int totalFruit(int[] fruits) {
-        int n = fruits.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        int max = 0;
-        int i = 0, j = 0;
+    public int totalFruit(int[] fr) {
+        int f = fr[0], s = -1;
+        int l = 0, h = 1;
+        int cc = 1, mxc = 1;
 
-        while (j < n) {
-            // Add current fruit to the map
-            map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
+        int lastCount = 1;
 
-            // If we have more than 2 types, shrink window from left
-            while (map.size() > 2) {
-                map.put(fruits[i], map.get(fruits[i]) - 1);
-                if (map.get(fruits[i]) == 0) {
-                    map.remove(fruits[i]); // Remove type with zero count
+        while (h < fr.length) {
+            if (fr[h] == f || fr[h] == s) {
+                cc++;
+            } else {
+                if (s == -1) {
+                    s = fr[h];
+                    cc++;
+                } else {
+                    // shrink to last continuous block
+                    cc = lastCount + 1;
+                    l = h - lastCount;
+
+                    // IMPORTANT FIX:
+                    // last continuous fruit becomes one type
+                    f = fr[h - 1];
+                    s = fr[h];
                 }
-                i++; // Shrink window
+            }
+            
+            // update lastCount
+            if (fr[h] == fr[h - 1]) {
+                lastCount++;
+            } else {
+                lastCount = 1;
             }
 
-            // Update max length of valid window
-            max = Math.max(max, j - i + 1);
-            j++; // Expand window
+            mxc = Math.max(mxc, cc);
+            h++;
         }
 
-        return max;
+        return mxc;
     }
 }
