14ee458569ad51ea39a31b2aef86c5433063d70a
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-03 16:57:41 +0530

    Time: 8 ms (17.48%), Space: 44.6 MB (94.08%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -3,19 +3,14 @@ class Solution {
         HashMap<Character, Integer> freq = new HashMap<>();
         int l = 0, h = 0, mxl = l;
         while(h<s.length()){
-            int k = h - l + 1;
             char ch = s.charAt(h);
             freq.put(ch,freq.getOrDefault(ch,0)+1);
 
-
-            while(freq.size()<k){
-                char c = s.charAt(l);
-                freq.put(c,freq.get(c)-1);
-                if(freq.get(c)==0) freq.remove(c);
-                l++;
-                k = h - l + 1;
+            while (freq.get(ch) > 1) {
+                char left = s.charAt(l++);
+                freq.put(left, freq.get(left) - 1);
             }
-            if(freq.size()==k) mxl = Math.max(mxl,k);
+            mxl = Math.max(mxl,h-l+1);
             h++;
         }
         return mxl;
