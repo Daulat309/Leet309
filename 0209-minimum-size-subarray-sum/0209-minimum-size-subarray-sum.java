b4363f46a0d3b58dd05c3216d7b97a1930760a39
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-04 22:28:34 +0530

    Time: 1 ms (99.94%), Space: 62 MB (70.17%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,16 +1,35 @@
 class Solution {
     public int minSubArrayLen(int x, int[] n) {
-        int l = 0, h = 0, s = 0, mnl = 1000000;
-        while(h<n.length){
+        int mnc = 1000000;
+        int l = 0, h = 0;
+        int s = 0;
+        while (h < n.length) {
             s += n[h];
-            if(s>=x) mnl = Math.min(mnl, h-l+1);
-            while(s>x&&l<=h){
+            while (s >= x) {
+                mnc = Math.min(mnc, h - l + 1);
                 s -= n[l];
                 l++;
-                if(s>=x) mnl = Math.min(mnl, h-l+1);
             }
-            h++;
+            h++;   
         }
-        return mnl==1000000?0:mnl;
+
+
+
+
+
+
+        // for(int i = 0;i<n.length;i++){
+        //     int s = 0;
+        //     int l = i;
+        //     while(l<n.length){
+        //         s += n[l];
+        //         if(s>=x){
+        //             mnc = Math.min((l-i+1),mnc);
+        //             break;
+        //         }
+        //         l++;
+        //     }
+        // }
+        return mnc==1000000?0:mnc;
     }
 }
