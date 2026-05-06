59b78818366e3cca46222b6412380914e07a3bed
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-06 10:58:58 +0530

    Time: 0 ms (100%), Space: 47 MB (13.76%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,18 +11,30 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode rabit = head;
-        ListNode turtle = head;
-        while(rabit!=null&&rabit.next!=null){
-            rabit = rabit.next.next;
-            turtle = turtle.next;
-            if(rabit==turtle) break;
+        ListNode sl = head;
+        ListNode fs = head;
+
+        while(fs!=null&&fs.next!=null){
+            sl = sl.next;
+            fs = fs.next.next;
+            if(sl==fs){
+            sl = head;
+            while(sl!=fs){
+                sl = sl.next;
+                fs = fs.next;
+            }
+            return sl;
+            }
         }
-        if(rabit==null||rabit.next==null) return null;
-        while(head!=turtle){
-            head = head.next;
-            turtle = turtle.next;
-        }
-        return head;
+        return null;
     }
 }
+
+
+
+// Meeting point for slow pointer is L1 + L2 for fast it is L1 + L2 + nC
+// 2 ( L1 + L2 ) = L1 + L2 + nC
+// L1 + L2 = nC  //n==1
+// L1 = C - L2
+// After meeting slow need to reach starting point is L2 - C which is L1
+// After meeting slow start from initial and fast start from same point they will meet at starting point
