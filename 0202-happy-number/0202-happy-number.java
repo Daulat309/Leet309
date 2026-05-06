cc516b10b0548f67855954d9607fb1ca19537c08
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-06 11:14:02 +0530

    Time: 1 ms (79.96%), Space: 42.3 MB (79.07%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,22 +1,21 @@
 class Solution {
     public boolean isHappy(int n) {
-        int slow = n, fast = n;
-
-        do {
-            slow = next(slow);
-            fast = next(next(fast));
-        } while (slow != fast);
-
-        return slow == 1;
+        int sl = n;
+        int fs = n;
+        do{
+            sl = sqr(sl);
+            fs = sqr(sqr(fs));
+        }while(sl!=fs);
+        return sl==1;
     }
 
-    private int next(int n) {
-        int sum = 0;
-        while (n > 0) {
+    public int sqr(int n){
+        int s = 0;
+        while(n>0){
             int d = n % 10;
-            sum += d * d; 
+            s += d*d;
             n /= 10;
         }
-        return sum;
+        return s;
     }
 }
