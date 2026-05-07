5dbb05daeefbe1047b1f85408eebc43a143ab5d9
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-07 08:53:48 +0530

    Time: 1 ms (99.97%), Space: 81 MB (5.73%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,9 +1,9 @@
 class Solution {
     public int maxSubArray(int[] a) {
-        int h = 1, s = a[0];
+        int h = 1, s = a[0], cur = a[0];
         while(h<a.length){
-            a[h] = Math.max(a[h-1]+a[h], a[h]);
-            s = Math.max(s,a[h]);
+            cur = Math.max(a[h],cur+a[h]);
+            s = Math.max(s,cur);
             h++;
         }
         return s;
