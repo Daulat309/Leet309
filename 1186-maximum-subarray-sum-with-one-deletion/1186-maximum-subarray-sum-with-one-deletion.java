571447323d8cf6a012455470e924ce5b4adb4317
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-08 20:04:16 +0530

    Time: 6 ms (89.81%), Space: 55.5 MB (84.43%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.java
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.java
@@ -4,10 +4,7 @@ class Solution {
         int nd = a[0];
         int h = 1,mx = a[0];
         while(h<a.length){
-            if(od+a[h]<nd){
-                od = nd;
-            }
-            else od += a[h];
+            od = Math.max(a[h]+od, nd);
             nd = Math.max(nd+a[h],a[h]);
             mx = Math.max(mx,Math.max(od,nd));
             h++;
