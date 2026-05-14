0413b3527ee5611f10e435aa3fea9ca6b3d759c7
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-14 08:28:22 +0530

    Time: 1 ms (91.05%), Space: 44.8 MB (64.57%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,12 +1,19 @@
 class Solution {
     public boolean isGood(int[] nums) {
-        Arrays.sort(nums);
+        //Arrays.sort(nums);
+        int cnt = 0;
+        int mn = nums[0];
+        int mx = nums[0];
         int n = nums.length;
         int s = nums[0];
+        if(nums[0]==n-1) cnt++;
         for(int i = 1;i<nums.length;i++){
             s += nums[i];
             s -= i;
+            if(nums[i]==n-1) cnt++;
+            mn = Math.min(mn,nums[i]);
+            mx = Math.max(mx,nums[i]);
         }
-        return s==n-1&&nums[0]==1&&nums[n-1]==nums[n-2];
+        return s==n-1&&mn==1&&mx==n-1&&cnt==2;
     }
 }
