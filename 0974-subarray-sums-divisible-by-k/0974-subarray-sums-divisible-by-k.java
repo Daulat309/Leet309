3b2cb16b3a60cc680fd141a1a793ba2c9c1b4955
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-24 17:13:45 +0530

    Time: 24 ms (69.44%), Space: 54.6 MB (12.08%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -1,34 +1,15 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-
-        int sum = 0;
-        int cnt = 0;
-
+        int s = 0, cnt = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
-
-        map.put(0, 1);
-
-        for (int num : nums) {
-
-            sum += num;
-
-            // raw java modulo
-            int rem = sum % k;
-
-            // search same remainder
-            cnt += map.getOrDefault(rem, 0);
-
-            // search equivalent negative/positive class
-            if (rem >= 0) {
-                cnt += map.getOrDefault(rem - k, 0);
-            } else {
-                cnt += map.getOrDefault(rem + k, 0);
-            }
-
-            // store raw remainder
-            map.put(rem, map.getOrDefault(rem, 0) + 1);
+        map.put(0,1);
+        for(int i = 0;i<nums.length;i++){
+            s += nums[i];
+            int m = (s%k + k)%k;
+            int f = map.getOrDefault(m,0);
+            cnt += f;
+            map.put(m,map.getOrDefault(m,0)+1);
         }
-
         return cnt;
     }
 }
