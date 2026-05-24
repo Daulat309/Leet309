a674afac3aed48359bba5e7476a807e1de2bef70
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-24 08:31:21 +0530

    Time: 1 ms (98.48%), Space: 47.4 MB (58.27%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -4,9 +4,8 @@ class Solution {
         for(int i : nums) s += i;
         int l = 0;
         for(int i = 0;i<nums.length;i++){
-            if(i>0)l += nums[i-1];
-            s -= nums[i];
-            if(s==l) return i;
+            if(s-l-nums[i]==l) return i;
+            l += nums[i];
         }
         return -1;
     }
