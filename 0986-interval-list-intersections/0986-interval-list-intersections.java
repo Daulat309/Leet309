105e396c3ee7ab33ba383427a3ea8f9612b94b9f
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-25 15:38:16 +0530

    Time: 5 ms (14.63%), Space: 47.5 MB (17.81%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.java
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -3,18 +3,17 @@ class Solution {
         int in1 = 0, in2 = 0;
         ArrayList<int[]> list = new ArrayList<>();
         if(fl.length==0||sl.length==0) return new int[][]{};
-        
         while(in1<fl.length&&in2<sl.length){
             int s1 = fl[in1][0], e1 = fl[in1][1];
             int s2 = sl[in2][0], e2 = sl[in2][1];
-
-            int s = Math.max(s1,s2);
-            int e = Math.min(e1,e2);
+            int s = Math.max(s1,s2), e = Math.min(e1,e2);
             if(s<=e) list.add(new int[]{s,e});
-            if(fl[in1][1]<sl[in2][1]){
+            if(e1<e2){
                 in1++;
             }
-            else in2++;
+            else{
+                in2++;
+            }
         }
 
         int[][] r = new int[list.size()][2];
