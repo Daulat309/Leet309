f2d1cdf52ffe5ab775771cbbd8389a2a79a10299
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-25 21:51:35 +0530

    Time: 4 ms (5.07%), Space: 51 MB (5.5%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,13 +1,7 @@
 class Solution {
     public void reverseString(char[] s) {
-        rev(s,0,s.length-1);
-    }
-
-    public void rev(char[] s, int l, int h){
-        if(l>h) return;
-        char t = s[l];
-        s[l] = s[h];
-        s[h] = t;
-        rev(s,l+1,h-1);
+        Stack<Character> st = new Stack<>();
+        for(char c : s) st.push(c);
+        for(int i = 0;i<s.length;i++) s[i] = st.pop();
     }
 }
