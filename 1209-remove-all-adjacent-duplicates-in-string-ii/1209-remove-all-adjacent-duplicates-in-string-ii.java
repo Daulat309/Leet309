a3378822b35a2940e2bf4fed398e94692f386daa
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-26 18:20:28 +0530

    Time: 36 ms (29.24%), Space: 47.6 MB (25.45%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -13,24 +13,13 @@ class Solution {
         int cnt = 0;
         for(int i = 0;i<s.length();i++){
             char c = s.charAt(i);
-            Pair p = null;
-            if(!st.isEmpty()) p = st.peek();
-            if(!st.isEmpty()&&p.ch==c){
-                if(p.num==k-1){
-                    //while(!st.isEmpty()&&st.peek().ch==c){
-                        st.pop();
-                    //}
-                }
-                else{
-                    st.peek().num++;
-                }
+            if(!st.isEmpty()&&st.peek().ch==c){
+                if(st.peek().num==k-1) st.pop();
+                else st.peek().num++;
             }
-            else{
-                st.push(new Pair(c,1));
-            }
+            else st.push(new Pair(c,1));
         }
         StringBuilder sb = new StringBuilder();
-        //int l = st.size();
         while(!st.isEmpty()){
             while(st.peek().num>0){
                 sb.append(st.peek().ch);
