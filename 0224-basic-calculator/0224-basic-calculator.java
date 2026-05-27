9432fc2e44ef24780b94b9eb1a260a8978a69f76
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-27 11:12:01 +0530

    Time: 9 ms (93.11%), Space: 46.7 MB (25.96%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -12,40 +12,24 @@ class Solution {
                 sn = 1;
             }
             else if(c==')'){
-                n *= sn;
-                r += n;
+                r += n*sn;
                 n = 0;
                 sn = 1;
                 if(!st.isEmpty()){
-                    sn = st.pop();
-                    r *= sn;
+                    r *= st.pop();
                     r += st.pop();
                     sn = 1;
                 }
             }
             else if(c=='+'||c=='-'){
-                n *= sn;
-                System.out.println("Before r : "+r);
-                r += n;
-                System.out.println("After r : "+r);
+                r += n*sn;
                 n = 0;
                 sn = c=='+'?1:-1;
             }
             else{
-                int d = c - '0';
-                System.out.println("First n : "+n+" Digit : "+d);
-                n = n*10 + d;
-                System.out.println("After n : "+n);
+                n = n*10 + c - '0';
             }
         }
-        if(n!=0) {
-                n *= sn;
-                System.out.println("Before r : "+r);
-                r += n;
-                System.out.println("After r : "+r);
-                n = 0;
-                sn = 1;
-        }
-        return r;
+        return r + sn*n;
     }
 }
