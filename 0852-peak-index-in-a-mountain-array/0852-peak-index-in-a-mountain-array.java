cf3f00ba70236392ff07488b53277da98baf41c3
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-30 12:09:55 +0530

    Time: 0 ms (100%), Space: 80.2 MB (56.7%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,9 +1,6 @@
 class Solution {
     public int peakIndexInMountainArray(int[] a) {
-        if(a[0]>a[1]) return a[0];
-        if(a[a.length-1]>a[a.length-2]) return a[a.length-1];
-
-        int l = 1, h = a.length-2;
+        int l = 0, h = a.length-1;
         while(l<h){
             int m = l + ( h - l )/2;
             if(a[m]>a[m+1]) h = m;
