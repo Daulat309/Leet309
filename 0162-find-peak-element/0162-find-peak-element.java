fad9e15051708f445cc3d853e6a810e830c092f2
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-30 12:31:25 +0530

    Time: 0 ms (100%), Space: 44.3 MB (17.9%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,11 +1,14 @@
 class Solution {
-    public int findPeakElement(int[] n) {
-        int l = 0, h = n.length-1;
-        while(l<=h){
-            int m = l +(h-l)/2;
-            if(m<h&&n[m]<n[m+1]) l = m + 1;
-            else if (0<m&&n[m]<n[m-1]) h = m-1;
-            else return m;
+    public int findPeakElement(int[] a) {
+        if(a.length==1||a[0]>a[1]) return 0;
+        if(a[a.length-1]>a[a.length-2]) return a.length-1;
+
+        int l = 1, h = a.length-2;
+        while(l<h){
+            int m = l + (h-l) / 2;
+            if(a[m-1]<a[m]&&a[m]>a[m+1]) return m;
+            if(a[m]>a[m+1]) h = m - 1;
+            else l = m + 1;
         }
         return l;
     }
