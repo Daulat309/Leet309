2590913035d4ea197bebcc1868d1e5d17b597968
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-05-31 08:48:56 +0530

    Time: 26 ms (81.52%), Space: 107.6 MB (16.4%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,11 +1,10 @@
 class Solution {
-    public boolean asteroidsDestroyed(int m, int[] a) {
+    public boolean asteroidsDestroyed(int mass, int[] a) {
         Arrays.sort(a);
-        long mass = m;
         for(int i = 0;i<a.length;i++){
             if(a[i]>mass) return false;
             mass += a[i];
-            //if(mass>100000) mass = 100000;
+            if(mass>100000) mass = 100000;
         }
         return true;
     }
