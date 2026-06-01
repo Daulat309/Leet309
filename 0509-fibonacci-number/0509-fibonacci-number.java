e7a6f672eea2b8e95d6ba3ea02b2a479c4b3cdca
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-01 19:31:53 +0530

    Time: 0 ms (100%), Space: 42.2 MB (32.68%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,17 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+        int[] a = new int[n+1];
+        Arrays.fill(a,-1);
+        return fibo(a,n);
+    }
+
+    public int fibo(int[] a, int n) {
+        if(a[n]!=-1) return a[n];
+        if(n<=1){
+            a[n] = n;
+            return a[n];
+        }
+        a[n] = fibo(a,n-1)+fibo(a,n-2);
+        return a[n];
     }
 }
