b449b3adadbec6020090a44c7d3c3f27dded54b5
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-06 09:19:07 +0530

    Time: 219 ms (21.14%), Space: 42.9 MB (75.61%) - LeetHub

---
+++ 3179-find-the-n-th-value-after-k-seconds/3179-find-the-n-th-value-after-k-seconds.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int valueAfterKSeconds(int n, int k) {
+        int[] a = new int[n];
+        Arrays.fill(a,1);
+        for(int j = 0;j<k;j++){
+            for(int i = 1;i<n;i++){
+                long l = (a[i-1]+a[i])%1000000007;
+                a[i] = (int)l;
+            }
+        }
+        return a[n-1];
+    }
+}
