447ad082d46065ce8ae3b6e6b65b535ceba86e3b
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-08 23:42:47 +0530

    Time: 0 ms (100%), Space: 43.4 MB (64.85%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,24 +1,29 @@
 class Solution {
     public List<String> letterCombinations(String d) {
-        String[] s = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(2,"abc");
+        map.put(3,"def");
+        map.put(4,"ghi");
+        map.put(5,"jkl");
+        map.put(6,"mno");
+        map.put(7,"pqrs");
+        map.put(8,"tuv");
+        map.put(9,"wxyz");
         List<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        call(d,s,0,sb,list);
+        insert(d,map,0,d.length(),list,new StringBuilder());
         return list;
     }
 
-
-    public void call(String o,String[] d, int idx, StringBuilder sb,List<String> list){
-        if(idx==o.length()){
+    public void insert(String d, HashMap<Integer,String> map, int idx, int n, List<String> list,StringBuilder sb){
+        if(idx==n){
             list.add(sb.toString());
             return;
         }
 
-        String s = d[o.charAt(idx)-'0'];
-
+        String s = map.get(d.charAt(idx)-'0');
         for(int i = 0;i<s.length();i++){
             sb.append(s.charAt(i));
-            call(o,d,idx+1,sb,list);
+            insert(d,map,idx+1,n,list,sb);
             sb.deleteCharAt(sb.length()-1);
         }
         return;
