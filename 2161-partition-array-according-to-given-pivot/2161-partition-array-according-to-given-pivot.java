0c2d514c41a2b14240073fe017680263d9c6a994
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-08 20:27:55 +0530

    Time: 5 ms (73.56%), Space: 158.9 MB (88.34%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,31 +1,32 @@
 class Solution {
     public int[] pivotArray(int[] a, int p) {
         int c = 0, idx = 0;
+        int[] r = new int[a.length];
         for(int i : a){
             if(i<p) idx++;
             if(i==p) c++;
         }
         int l = 0,h = idx+c;
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0;i<a.length;i++) list.add(0);
+        //List<Integer> list = new ArrayList<>();
+        //for(int i = 0;i<a.length;i++) list.add(0);
         for(int i = 0;i<a.length;i++){
             if(a[i]==p){
-                list.set(idx,a[i]);
+                r[idx] = a[i];
                 idx++;
             }
             else if(a[i]<p){
-                list.set(l,a[i]);
+                r[l] = a[i];
                 l++;
             }
             else{
-                list.set(h,a[i]);
+                r[h] = a[i];
                 h++;
             }
         }
-        System.out.println(list);
-        for(int i = 0;i<a.length;i++){
-            a[i] = list.get(i);
-        }
-        return a;
+        // System.out.println(list);
+        // for(int i = 0;i<a.length;i++){
+        //     a[i] = list.get(i);
+        // }
+        return r;
     }
 }
