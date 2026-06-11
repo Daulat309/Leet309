9caeb501deab6326479717c780d63aa6aca5f62e
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-11 09:59:23 +0530

    Time: 2 ms (95.09%), Space: 45.9 MB (33.2%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -7,18 +7,27 @@ class Solution {
     }
 
     public void per(List<List<Integer>> list, List<Integer> l, int[] a, int s,int t,int idx){
-        if(s>=t){
+        if(idx==a.length){
             if(s==t) list.add(new ArrayList<>(l));
             return;
         }
 
-        for(int i = idx;i<a.length;i++){
-            l.add(a[i]);
-            s += a[i];
-            per(list,l,a,s,t,i);
-            s -= a[i];
-            l.remove(l.size()-1);  
+        per(list,l,a,s,t,idx+1);
+        if(s+a[idx]<=t){
+            s += a[idx];
+            l.add(a[idx]);
+            per(list,l,a,s,t,idx);
+            s -= a[idx];
+            l.remove(l.size()-1);
         }
+
+        // for(int i = idx;i<a.length;i++){
+        //     l.add(a[i]);
+        //     s += a[i];
+        //     per(list,l,a,s,t,i);
+        //     s -= a[i];
+        //     l.remove(l.size()-1);  
+        // }
         return;   
     }
 }
