51a732cb1a0012cae5f75a5e0b4e68282a7a4380
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-13 22:30:07 +0530

    Time: 1 ms (83.22%), Space: 43.9 MB (59.72%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -16,29 +16,32 @@
 class Solution {
     boolean is = true;
     public boolean isCompleteTree(TreeNode root) {
-        Queue<TreeNode> q = new ArrayDeque<>();
+        Queue<TreeNode> q = new LinkedList<>();
         if(root==null) return true;
         q.offer(root);
-        return isc(q);
-    }
-
-    public boolean isc(Queue<TreeNode> q) {
         while(!q.isEmpty()){
-            int l = q.size();
-            while(l-->0){
-                TreeNode tn = q.poll();
-                if(tn.left!=null){
-                    if(!is) return false;
-                    q.offer(tn.left);
-                }
-                else is = false;
-                if(tn.right!=null){
-                    if(!is) return false;
-                    q.offer(tn.right);
-                }
-                else is = false;
-
+            TreeNode tn = q.peek();
+            q.poll();
+            if(tn==null) is = false;
+            else{
+                if(!is) return false;
+                q.offer(tn.left);
+                q.offer(tn.right);
             }
+            //int l = q.size();
+            // while(l-->0){
+            //     TreeNode tn = q.poll();
+            //     if(tn.left!=null){
+            //         if(!is) return false;
+            //         q.offer(tn.left);
+            //     }
+            //     else is = false;
+            //     if(tn.right!=null){
+            //         if(!is) return false;
+            //         q.offer(tn.right);
+            //     }
+            //     else is = false;
+            // }
         }
         return true;
     }
