e20fbba26d5281493edccbff02cbcde36c5adc4a
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-13 07:48:03 +0530

    Time: 0 ms (100%), Space: 45 MB (49.19%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        int[] is = new int[3];
+        int[] is = new int[2];
         check(root, targetSum, is);
         return is[1]==1;
     }
@@ -23,13 +23,11 @@ class Solution {
     public void check(TreeNode root, int x, int[] is){
         if(root==null) return;
         is[0] += root.val;
-        is[2] = root.val;
         if(root.left==null&&root.right==null){
             if(is[0]==x) is[1] = 1;
         }
         check(root.left, x, is);
         check(root.right, x, is);
-        System.out.println(is[2]);
         is[0] -= root.val;
     }
 }
