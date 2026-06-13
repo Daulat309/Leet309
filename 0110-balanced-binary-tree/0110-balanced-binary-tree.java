f362499c09e1ab08a701fcc079de2bb203b72ada
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-13 17:27:13 +0530

    Time: 0 ms (100%), Space: 45.6 MB (40.7%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,23 +14,16 @@
  * }
  */
 class Solution {
+    boolean is = true;
     public boolean isBalanced(TreeNode root) {
-        return dfsHeight(root) != -1;
+        int c = check(root);
+        return is;
     }
-
-    private int dfsHeight(TreeNode node) {
-        if (node == null) return 0;
-
-        int leftHeight = dfsHeight(node.left);
-        if (leftHeight == -1) return -1;
-
-        int rightHeight = dfsHeight(node.right);
-        if (rightHeight == -1) return -1;
-
-        if (Math.abs(leftHeight - rightHeight) > 1) {
-            return -1;
-        }
-
-        return 1 + Math.max(leftHeight, rightHeight);
+    public int check(TreeNode root){
+        if(root==null) return 0;
+        int l = check(root.left);
+        int r = check(root.right);
+        if(Math.abs(l-r)>1) is = false;
+        return Math.max(l,r)+1;
     }
 }
