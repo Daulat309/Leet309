8c8d922c861476c9488f406953a8d4f28adcc3fd
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-16 07:41:43 +0530

    Time: 3 ms (100%), Space: 55.3 MB (48.53%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -1,17 +1,17 @@
 class Solution {
     public String processStr(String s) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0;i<s.length();i++){
-            if(s.charAt(i)=='*'){
+        for(char c : s.toCharArray()){
+            if(c=='*'){
                 if(!sb.isEmpty()) sb.deleteCharAt(sb.length()-1);
             }
-            else if(s.charAt(i)=='#'){
+            else if(c=='#'){
                 if(!sb.isEmpty()) sb.append(sb.toString());
             }
-            else if(s.charAt(i)=='%'){
+            else if(c=='%'){
                 if(!sb.isEmpty()) sb.reverse();
             }
-            else sb.append(s.charAt(i));
+            else sb.append(c);
         }
         return sb.toString();
     }
