b350662e9d96a988dbfd0c7815eac19d64cc6e6d
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-17 22:57:29 +0530

    Time: 0 ms (100%), Space: 43.4 MB (95.78%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,19 +1,22 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        return bS(0,nums.length-1,nums,target);
-    }
+    public int search(int[] a, int x) {
+        int l = 0, h = a.length-1;
+        while(l<=h){
+            int m = l + (h-l)/2;
+            if(a[m]==x) return m;
+            if(a[m]>a[a.length-1]){
+                if(a[l]<=x&&x<a[m]) h = m-1;
+                else l = m + 1;
+            }
+            else{
+                if(x<=a[a.length-1]){
+                    if(x<a[m]) h = m-1;
+                    else l = m+1;
 
-    public int bS(int l, int h, int[] a, int x){
-        if(l>h) return -1;
-        int m = l + (h-l)/2;
-        if(a[m]==x) return m;
-        if(a[l]<=a[m]){
-            if(a[l]<=x&&x<a[m]) return bS(l,m-1,a,x);
-            return bS(m+1,h,a,x);
+                }
+                else h = m - 1;
+            }
         }
-        else{
-            if(a[m]<x&&x<=a[h]) return bS(m+1,h,a,x);
-           return bS(l,m-1,a,x);
-        }
-    } 
+        return -1;
+    }
 }
