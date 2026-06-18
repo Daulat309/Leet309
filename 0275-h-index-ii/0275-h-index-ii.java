6d048db7edf558a2109f7bd850bbf6627f66730a
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-18 22:13:11 +0530

    Time: 4 ms (7.44%), Space: 50.2 MB (47.11%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int hIndex(int[] c) {
+        int mx = Integer.MAX_VALUE;
+        for(int i : c) mx = Math.max(mx,i);
+        int l = 0, h = mx, res = 0;
+        while(l<=h){
+            int m = l + (h-l)/2;
+            if(mikecheck(c,m)){
+                res = m;
+                l = m + 1;
+            }
+            else h = m - 1;
+        }
+        return res;
+    }
+
+    public boolean mikecheck(int[] a, int m){
+        int cnt = 0;
+        for(int i = 0;i<a.length;i++){
+            if(a[i]>=m) cnt++;
+            if(cnt>=m) return true;
+        }
+        return false;
+    }
+}
+
