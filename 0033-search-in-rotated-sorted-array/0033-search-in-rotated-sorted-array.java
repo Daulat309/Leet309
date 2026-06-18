6ad86801dfbfc903c076332760737161ea7f354c
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-18 09:43:50 +0530

    Time: 0 ms (100%), Space: 43.9 MB (46.02%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,18 +1,38 @@
 class Solution {
     public int search(int[] a, int x) {
         int l = 0, h = a.length-1;
+        // while(l<=h){
+        //     int m = l + (h-l)/2;
+        //     if(a[m]==x) return m;
+        //     if(a[m]>a[a.length-1]){
+        //         if(a[l]<=x&&x<a[m]) h = m-1;
+        //         else l = m + 1;
+        //     }
+        //     else{
+        //         if(x<=a[h]&&x>a[m]) l = m+1;
+        //         else h = m - 1;
+        //     }
+        // }
+
         while(l<=h){
             int m = l + (h-l)/2;
             if(a[m]==x) return m;
             if(a[m]>a[a.length-1]){
-                if(a[l]<=x&&x<a[m]) h = m-1;
-                else l = m + 1;
+                if(a[m]<x) l = m+1;
+                else{
+                    if(a[0]>x) l = m+1;
+                    else h = m-1;
+                }
             }
             else{
-                if(x<=a[h]&&x>a[m]) l = m+1;
-                else h = m - 1;
+                if(x<a[m]) h = m -1;
+                else{
+                    if(x>a[a.length-1]) h = m -1;
+                    else l = m + 1;
+                }
             }
         }
+
         return -1;
     }
 }
