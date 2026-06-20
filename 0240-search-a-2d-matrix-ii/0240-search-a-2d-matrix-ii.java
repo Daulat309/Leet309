5d938938ea8bcb1254e98d8ac2f23a96cdd2cddb
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-20 16:00:23 +0530

    Time: 3 ms (88.69%), Space: 48.5 MB (9.74%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,15 +1,11 @@
 class Solution {
     public boolean searchMatrix(int[][] a, int x) {
-        for(int i = 0;i<a.length;i++){
-            int l = 0, h = a[0].length-1;
-            if ((a[i][l] <= x) && (x <= a[i][h])){
-                while(l<=h){
-                    int m = l + (h-l)/2;
-                    if(a[i][m]==x) return true;
-                    if(a[i][m]>x) h = m-1;
-                    else l = m + 1;
-                }
-            }
+
+        int r = 0, c = a[0].length-1;
+        while(r<a.length&&c>=0){
+            if(a[r][c]==x) return true;
+            if(a[r][c]>x) c--;
+            else r++;
         }
         return false;
     }
