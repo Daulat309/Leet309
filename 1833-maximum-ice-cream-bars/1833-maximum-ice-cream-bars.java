b1ca22116f466f44e7e07e1f73b7d0890a595b79
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-21 05:40:06 +0530

    Time: 37 ms (56.85%), Space: 84.3 MB (5.8%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int cnt = 0;
+        for(int i = 0;i<costs.length&&coins>=0;i++){
+            coins -= costs[i];
+            if(coins>=0) cnt++;
+        }
+        return cnt;
+    }
+}
