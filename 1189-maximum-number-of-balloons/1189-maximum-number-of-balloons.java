8376f0ffe151c394336d79c83746a2bb6a7851e7
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-22 08:11:49 +0530

    Time: 7 ms (53.16%), Space: 43.6 MB (54.89%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -8,13 +8,13 @@ class Solution {
                 map.put(c,map.getOrDefault(c,0)+1);
             }
         }
-        for(int i = 0;i<text.length();i++){
-            char c = text.charAt(i);
-            if(c=='b'||c=='a'||c=='l'||c=='o'||c=='n'){
-                if(c=='l'||c=='o') mn = Math.min(map.get(c)/2,mn);
-                else mn = Math.min(map.get(c),mn);
-            }
+        if(map.size()!=5) return 0;
+        String s= "balon";
+        for(int i = 0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='l'||c=='o') mn = Math.min(map.get(c)/2,mn);
+            else mn = Math.min(map.get(c),mn);
         }
-        return map.size()==5?mn:0;
+        return mn;
     }
 }
