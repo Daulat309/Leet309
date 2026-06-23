e9b3f450b8213b114bbc11eb7a2a689850ad4465
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-23 07:43:07 +0530

    Time: 52 ms (10.32%), Space: 52.2 MB (89.65%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,25 +1,16 @@
 class Solution {
     public int[][] kClosest(int[][] p, int k) {
-        HashMap<Integer, Double> map = new HashMap<>();
         Queue<Integer> q = new PriorityQueue<>(
             (a,b)->{
-                if(map.get(a).equals(map.get(b))) return b.compareTo(a);
-                return map.get(b).compareTo(map.get(a));
+                double d2 = (double)p[b][0]*p[b][0]+(p[b][1]*p[b][1]);
+                double d1 = (double)p[a][0]*p[a][0]+(p[a][1]*p[a][1]);
+                return Double.compare(d2,d1);
             }
         );
 
         for(int i = 0;i<p.length;i++){
-            int x = p[i][0];
-            int y = p[i][1];
-            double d = (double)Math.sqrt((x*x)+(y*y));
-            map.put(i,d);
-        }
-
-        for(Integer key : map.keySet()){
-            q.offer(key);
-            if(q.size()>k){
-                q.poll();
-            }
+            q.offer(i);
+            if(q.size()>k) q.poll();
         }
         int[][] res = new int[k][2];
         int i = k - 1;
