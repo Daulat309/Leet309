815652e8f5a0046d31d9dce746a587f26f95aae1
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-23 17:30:43 +0530

    Time: 1393 ms (5.01%), Space: 145.1 MB (5.1%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -1,13 +1,12 @@
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] p, int[] c) {
-        int n = 0;
         Queue<Integer> q = new PriorityQueue<>(
             (a,b) -> {
                 if(p[a]==p[b]) return c[a]-c[b];
                 return p[b]-p[a];
             }
         );
-        
+
         for(int i = 0;i<p.length;i++){
             if(p[i]>0){
                 q.offer(i);
@@ -16,10 +15,10 @@ class Solution {
         
         ArrayList<Integer> list = new ArrayList<>();
         while(!q.isEmpty()){
-            if(n>=k) return w;
+            if(k==0) return w;
             int idx = q.poll();
             if(c[idx]<=w){
-                n++;
+                k--;
                 w += p[idx];
                 for(int i = 0;i<list.size();i++){
                     q.offer(list.get(i));
