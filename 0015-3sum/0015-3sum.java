01c10a33390fa2d9ab6cb7f638777f3e05e04045
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-25 17:30:39 +0530

    Time: 37 ms (24.45%), Space: 59.1 MB (59.6%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,26 +1,25 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] n) {
+    public List<List<Integer>> threeSum(int[] a) {
+        Arrays.sort(a);
         List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(n);
+        for(int i = 0;i<a.length-2;i++){
 
-        for(int i = 0;i<n.length-2;i++){
-            if(i > 0 && n[i] == n[i - 1]) continue;
-            int x = -n[i];
-            int l = i + 1, h = n.length-1;
+            if(i>0&&a[i]==a[i-1]) continue;
+            int x = -1*a[i];
+            int l = i + 1, h = a.length-1;
             while(l<h){
-                int sm = n[l]+n[h];
+                int sm = a[l]+a[h];
                 if(sm==x){
-                    list.add(Arrays.asList(n[i],n[l],n[h]));
+                    list.add(new ArrayList<>(Arrays.asList(a[i], a[l], a[h])));
                     l++;
                     h--;
-                    while(l<h&&n[l]==n[l-1]) l++;
-                    while(l<h&&n[h]==n[h+1]) h--;
+                    while(l<h&&a[l]==a[l-1]) l++;
+                    while(h>l&&a[h]==a[h+1]) h--;
                 }
                 else{
-                    if(sm>x) h--;
-                    else l++;
+                   if(sm>x) h--;
+                   else l++;
                 }
-
             }
         }
         return list;
