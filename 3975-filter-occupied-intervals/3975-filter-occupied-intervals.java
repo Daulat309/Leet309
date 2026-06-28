37978b0d72b13b3ba32019ed17ab65d705f1c0f8
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-28 10:01:47 +0530

    Time: 68 ms (98.97%), Space: 149.3 MB (97.18%) - LeetHub

--- 3975-filter-occupied-intervals/3975-filter-occupied-intervals.java
+++ 3975-filter-occupied-intervals/3975-filter-occupied-intervals.java
@@ -24,24 +24,16 @@ class Solution {
         }
         
         if(e<fs||s>fe){
-                list.add(Arrays.asList(s,e));
+            list.add(Arrays.asList(s,e));
         }
         else{
-                if(s<fs){
-                    list.add(Arrays.asList(s,fs-1));
-                }
-                if(e>fe){
-                    list.add(Arrays.asList(fe+1,e));
-                }
+            if(s<fs){
+                list.add(Arrays.asList(s,fs-1));
+            }
+            if(e>fe){
+                list.add(Arrays.asList(fe+1,e));
+            }
         }
-        // List<List<Integer>> ad = new ArrayList<>();
-        // for(int i = 0;i<list.size();i++){
-        //     s = list.get(i).get(0);
-        //     e = list.get(i).get(1);
-
-            
-        // }
-        //list.clear();
         return list;
     }
 }
