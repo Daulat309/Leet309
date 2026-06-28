0700767309c2ab70a8ffd418e102923239c87a0a
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-28 10:01:11 +0530

    Time: 67 ms (99.61%), Space: 149.4 MB (95.98%) - LeetHub

--- 3975-filter-occupied-intervals/3975-filter-occupied-intervals.java
+++ 3975-filter-occupied-intervals/3975-filter-occupied-intervals.java
@@ -7,45 +7,41 @@ class Solution {
         for(int i = 1;i<o.length;i++){
             if(o[i][0]<=e+1) e = Math.max(o[i][1],e);
             else{
+                if(e<fs||s>fe){
                 list.add(Arrays.asList(s,e));
+            }
+            else{
+                if(s<fs){
+                    list.add(Arrays.asList(s,fs-1));
+                }
+                if(e>fe){
+                    list.add(Arrays.asList(fe+1,e));
+                }
+            }
                 s = o[i][0];
                 e = o[i][1];
             }
         }
         
-        list.add(Arrays.asList(s,e));
-        List<List<Integer>> ad = new ArrayList<>();
-        boolean change = false;
-        for(int i = 0;i<list.size();i++){
-            s = list.get(i).get(0);
-            e = list.get(i).get(1);
-            if(e<fs||s>fe){
-                ad.add(Arrays.asList(s,e));
-            }
-            else{
+        if(e<fs||s>fe){
+                list.add(Arrays.asList(s,e));
+        }
+        else{
                 if(s<fs){
-                    ad.add(Arrays.asList(s,fs-1));
+                    list.add(Arrays.asList(s,fs-1));
                 }
                 if(e>fe){
-                    ad.add(Arrays.asList(fe+1,e));
+                    list.add(Arrays.asList(fe+1,e));
                 }
-            }
-            
         }
-        list.clear();
-        System.out.println(ad);
-        // if(ad.size()==0) return list;
-        // s = ad.get(0).get(0);
-        // e = ad.get(0).get(1);
-        // for(int i = 1;i<ad.size();i++){
-        //     if(e+1==ad.get(i).get(0)) e = ad.get(i).get(1);
-        //     else{
-        //         list.add(Arrays.asList(s,e));
-        //         s = ad.get(i).get(0);
-        //         e = ad.get(i).get(1);
-        //     }
+        // List<List<Integer>> ad = new ArrayList<>();
+        // for(int i = 0;i<list.size();i++){
+        //     s = list.get(i).get(0);
+        //     e = list.get(i).get(1);
+
+            
         // }
-        // list.add(Arrays.asList(s,e));
-        return ad;
+        //list.clear();
+        return list;
     }
 }
