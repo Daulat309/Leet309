b40d9822b1372528051f90baec4e2454f825e002
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-29 07:33:17 +0530

    Time: 10 ms (34.69%), Space: 47 MB (86.45%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,41 +1,51 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int vally = nums.length;
-        int[] a = new int[nums.length];
-        for(int i = 1;i<nums.length;i++){
-            if(Math.abs(nums[i-1])<Math.abs(nums[i])){
-                vally = i-1;
-                break;
-            }
+        for(int i = 0;i<nums.length;i++){
+            nums[i] = nums[i]*nums[i];
         }
-        int l = vally - 1, h = vally;
-        int i = 0;
-        while(l>=0&&h<a.length){
-            if(Math.abs(nums[l])>Math.abs(nums[h])){
-                a[i] = nums[h]*nums[h];
-                h++;
-            }
-            else{
-                a[i] = nums[l]*nums[l];
-                l--;
-            }
-            i++;
-        }
+        Arrays.sort(nums);
+        return nums;
 
-        while(h<a.length){
-            
-                a[i] = nums[h]*nums[h];
-                h++;
-            
-            i++;
-        }
 
-        while(l>=0){
+
+
+
+        // int vally = nums.length;
+        // int[] a = new int[nums.length];
+        // for(int i = 1;i<nums.length;i++){
+        //     if(Math.abs(nums[i-1])<Math.abs(nums[i])){
+        //         vally = i-1;
+        //         break;
+        //     }
+        // }
+        // int l = vally - 1, h = vally;
+        // int i = 0;
+        // while(l>=0&&h<a.length){
+        //     if(Math.abs(nums[l])>Math.abs(nums[h])){
+        //         a[i] = nums[h]*nums[h];
+        //         h++;
+        //     }
+        //     else{
+        //         a[i] = nums[l]*nums[l];
+        //         l--;
+        //     }
+        //     i++;
+        // }
+
+        // while(h<a.length){
             
-                a[i] = nums[l]*nums[l];
-                l--;
-                i++;
-        }
-        return a;
+        //         a[i] = nums[h]*nums[h];
+        //         h++;
+            
+        //     i++;
+        // }
+
+        // while(l>=0){
+            
+        //         a[i] = nums[l]*nums[l];
+        //         l--;
+        //         i++;
+        // }
+        // return a;
     }
 }
