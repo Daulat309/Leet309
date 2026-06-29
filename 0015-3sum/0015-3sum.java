0a9b500d21f7806975f81f74ca0819af83de73cc
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-29 08:03:26 +0530

    Time: 37 ms (24.53%), Space: 59.2 MB (45.51%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,32 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] a) {
-        Arrays.sort(a);
-        List<List<Integer>> list = new ArrayList<>();
-        for(int i = 0;i<a.length-2;i++){
-
-            if(i>0&&a[i]==a[i-1]) continue;
-            int x = -1*a[i];
-            int l = i + 1, h = a.length-1;
-            while(l<h){
-                int sm = a[l]+a[h];
-                if(sm==x){
-                    list.add(new ArrayList<>(Arrays.asList(a[i], a[l], a[h])));
-                    l++;
-                    h--;
-                    while(l<h&&a[l]==a[l-1]) l++;
-                    while(h>l&&a[h]==a[h+1]) h--;
-                }
-                else{
-                   if(sm>x) h--;
-                   else l++;
-                }
-            }
+    List<List<Integer>> list = new ArrayList<>();
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        list.clear();
+        System.out.println(Arrays.toString(nums));
+        for(int i = 0;i<nums.length-2;i++){
+            if(i>0&&nums[i]==nums[i-1]) continue;
+            int f = i;
+            int x = 0 - nums[f];
+            int s = i+1, t = nums.length-1;
+            check(nums,x,f,s,t);
         }
         return list;
+    }
+
+    public void check(int[] a, int x,int f, int l, int h){
+        while(l<h){
+            if(a[l]+a[h]==x){
+                list.add(Arrays.asList(a[f],a[l],a[h]));
+                l++;
+                h--;
+                while(l<h&&a[l]==a[l-1])l++;
+                while(l<h&&a[h]==a[h+1])h--;
+            }
+            else{
+                if((a[l]+a[h])>x) h--;
+                else l++;
+            }
+        }
     }
 }
