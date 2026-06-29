65612bfa17941ccbeae34aefb1610aee79eb1d9e
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-29 07:05:47 +0530

    Time: 2 ms (96%), Space: 48.2 MB (79.11%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,11 +1,46 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0;i<numbers.length;i++){
-            int r = target - numbers[i];
-            if(map.containsKey(r)) return new int[]{map.get(r)+1,i+1};
-            map.put(numbers[i],i);
+    public int[] twoSum(int[] n, int x) {
+        int l = 0, h = n.length-1;
+        int a = -1, b = -1;
+        while(l<h){
+            int sm = n[l]+n[h];
+            if(sm==x){
+                a = l;
+                b = h;
+                break;
+            }
+            if(sm>x) h--;
+            else l++;
         }
-        return  new int[]{-1,-1};
+
+        return new int[] {l+1,h+1};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i = 0;i<numbers.length;i++){
+        //     int r = target - numbers[i];
+        //     if(map.containsKey(r)) return new int[]{map.get(r)+1,i+1};
+        //     map.put(numbers[i],i);
+        // }
+        // return  new int[]{-1,-1};
     }
 }
