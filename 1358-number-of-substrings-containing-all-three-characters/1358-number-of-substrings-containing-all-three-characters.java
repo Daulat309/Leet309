b8e5e2161e4d2c7088de9091ac74258268265ab4
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-30 07:00:51 +0530

    Time: 16 ms (40.27%), Space: 46.3 MB (62.27%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,16 +1,28 @@
 class Solution {
     public int numberOfSubstrings(String s) {
         int cnt = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
+        
         int l = 0, h= 0;
+
+        int[] a = new int[3];
+        //HashMap<Character, Integer> map = new HashMap<>();
+
         while(h<s.length()){
-            map.put(s.charAt(h),map.getOrDefault(s.charAt(h),0)+1);
-            while(map.size()==3){
+
+            a[s.charAt(h)-'a']++;
+            while(a[0]>0&&a[1]>0&&a[2]>0){
                 cnt += s.length() - h;
-                map.put(s.charAt(l),map.get(s.charAt(l))-1);
-                if(map.get(s.charAt(l))==0) map.remove(s.charAt(l));
+                a[s.charAt(l)-'a']--;
                 l++;
             }
+
+            // map.put(s.charAt(h),map.getOrDefault(s.charAt(h),0)+1);
+            // while(map.size()==3){
+            //     cnt += s.length() - h;
+            //     map.put(s.charAt(l),map.get(s.charAt(l))-1);
+            //     if(map.get(s.charAt(l))==0) map.remove(s.charAt(l));
+            //     l++;
+            // }
             h++;
         }
         return cnt;
