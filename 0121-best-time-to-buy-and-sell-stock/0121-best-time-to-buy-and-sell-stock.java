d5fee7181af6ed92071c348db138f3548af01ace
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-30 13:02:13 +0530

    Time: 1 ms (99.94%), Space: 94.5 MB (50.8%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -3,8 +3,8 @@ class Solution {
         int min = prices[0];
         int res = 0;
         for(int i = 0;i<prices.length;i++){
-            res = Math.max(res, prices[i]-min);
             min = Math.min(prices[i],min);
+            res = Math.max(res, prices[i]-min);
         }
         return res;
     }
