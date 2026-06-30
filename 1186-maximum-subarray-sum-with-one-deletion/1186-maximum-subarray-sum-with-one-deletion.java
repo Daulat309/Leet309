a432fa958de5bc353da953b0c5f30178a7c3348a
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-30 11:47:09 +0530

    Time: 6 ms (91.33%), Space: 55.9 MB (47.61%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.java
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.java
@@ -1,11 +1,10 @@
 class Solution {
     public int maximumSum(int[] nums) {
         int nd = nums[0];
-        int od = nums[0];
+        int od = 0;
         int res = nums[0];
         for(int i = 1;i<nums.length;i++){
-            if(od==nd) od = Math.max(od,nums[i]);
-            else od = Math.max(nd,od+nums[i]);
+            od = Math.max(nd,od+nums[i]);
             nd = Math.max(nums[i],nd+nums[i]);
             res = Math.max(res,Math.max(nd,od));
         }
