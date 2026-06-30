5dbe48bec5c2893949bd1e6c6cb340b0b93e8c5d
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-30 08:09:16 +0530

    Time: 5 ms (41.09%), Space: 46.7 MB (78.24%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -21,39 +21,10 @@ class Solution {
         ListNode list = lists[i];
         ListNode sl = list;
         ListNode fs = list;
-        boolean cycle = false;
-        // while(fs!=null&&fs.next!=null){
-        //     fs = fs.next.next;
-        //     sl = sl.next;
-        //     if(sl==fs){
-        //         cycle = true;
-        //         break;
-        //     };
-        // }
-        ///if(!cycle){
-            while(list!=null){
+        while(list!=null){
             min.offer(new ListNode(list.val));
             list = list.next;
-            }
-        //}
-        // else{
-        //     sl = list;
-        //     while(sl!=fs){
-        //         sl = sl.next;
-        //         fs = fs.next;
-        //     }
-        //     while(sl!=fs){
-        //     min.offer(new ListNode(sl.val));
-        //     sl = sl.next;
-        //     }
-        //     min.offer(sl);
-        //     sl = sl.next;
-        //     while(sl!=fs){
-        //     min.offer(new ListNode(sl.val));
-        //     sl = sl.next;
-        //     }
-        // }
-        
+        }
         i++;
         }
         ListNode dummy = new ListNode(0);
