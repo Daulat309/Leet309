0a9dc1cf619f151fd0a686e1af22b2473ba21f4e
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-30 07:26:13 +0530

    Time: 0 ms (100%), Space: 44.6 MB (7.38%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -23,29 +23,7 @@ class Solution {
             }
             cur = cur.next;
         }
-        while(list1!=null){
-            // if(list1.val>list2.val){
-            //     cur.next = list2;
-            //     list2 = list2.next;
-            // }
-            //else {
-                cur.next = list1;
-                list1 = list1.next;
-            //}
-            cur = cur.next;
-        }
-
-        while(list2!=null){
-            //if(list1.val>list2.val){
-                cur.next = list2;
-                list2 = list2.next;
-            //}
-            // else {
-            //     cur.next = list1;
-            //     list1 = list1.next;
-            // }
-            cur = cur.next;
-        }
+        cur.next = (list1 != null) ? list1 : list2;
         return head.next;
     }
 }
