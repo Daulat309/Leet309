e36dd16117f1728daa1277ca346ada1862d7b29d
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-06-30 13:55:26 +0530

    Time: 0 ms (100%), Space: 42.4 MB (64.9%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,10 +1,10 @@
 class Solution {
     public int hammingWeight(int n) {
-        int s = 0;
+        int cnt = 0;
         while(n!=0){
-            s += n & 1;
-            n >>= 1;
+            cnt++;
+            n = n & (n-1);
         }
-        return s;
+        return cnt;
     }
 }
