95b55cb633e706e00a690990a9e3b56e77f3e474
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-03 18:04:16 +0530

    Time: 0 ms (100%), Space: 48.2 MB (84.25%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,13 +1,24 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        return bs(0,nums.length-1,nums,target);
-    }
-
-    public int bs(int l, int h, int[] a, int x){
-        if(l>h) return -1;
-        int m = l + (h-l)/2;
-        if(a[m]==x) return m;
-        if(a[m]<x) return bs(m+1,h,a,x);
-        return bs(l,m-1,a,x);
+    public int search(int[] a, int x) {
+        int l = 0, h = a.length-1;
+        while(l<=h){
+            int m = l + (h-l)/2;
+            if(a[m]==x) return m;
+            if(a[m]>a[a.length-1]){
+                if(x>a[m]) l = m + 1;
+                else{
+                    if(a[0]>x) l = m + 1;
+                    else h = m - 1;
+                }
+            }
+            else{
+                if(x<a[m]) h = m-1;
+                else{
+                    if(x>a[a.length-1]) h = m -1;
+                    else l = m + 1;
+                }
+            }
+        }
+        return -1;
     }
 }
