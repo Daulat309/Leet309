0a0fdb0ada5b65ce5f993468401f32067b69abf4
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-04 08:43:02 +0530

    Time: 0 ms (100%), Space: 50.3 MB (47.58%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -1,14 +1,12 @@
 class Solution {
     public int hIndex(int[] c) {
-        int l = 0, h = c.length-1, n = h + 1, res = 0;
+        int l = 0, h = c.length-1;
         while(l<=h){
             int m = l + (h-l)/2;
-            if(c[m]>=n-m){
-                res = n-m;
-                h = m - 1;
-            }
+            int rq = c.length - m;
+            if(c[m]>=rq) h = m -1;
             else l = m + 1;
         }
-        return res;
+        return c.length-l;
     }
 }
