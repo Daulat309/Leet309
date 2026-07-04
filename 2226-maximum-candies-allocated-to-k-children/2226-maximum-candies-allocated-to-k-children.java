b78589ba28fe6df2b4abf56c25da8750df4fd26f
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-04 09:31:29 +0530

    Time: 24 ms (94.32%), Space: 92.3 MB (9.79%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -1,11 +1,10 @@
 class Solution {
     public int maximumCandies(int[] c, long k) {
-        int l = 0, h = l,res = 0;
+        int l = 0, h = l;
         for(int i : c) h = Math.max(i,h);
         while(l<=h){
             int m = l + (h-l)/2;
             if(isp(c,m,k)){
-                res = m;
                 l = m + 1;
             }
             else h = m - 1;
