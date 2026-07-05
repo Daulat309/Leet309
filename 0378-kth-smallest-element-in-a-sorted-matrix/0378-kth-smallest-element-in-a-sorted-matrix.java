c8a4e82ecd43722cb7b023b87f215b26d99ac4d3
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-05 14:57:03 +0530

    Time: 10 ms (63.19%), Space: 51.8 MB (37.03%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,12 +1,11 @@
 class Solution {
-    
-    public int kthSmallest(int[][] a, int d) {
+    public int kthSmallest(int[][] a, int k) {
         int l = a[0][0], h = a[a.length-1][a[0].length-1];
         while(l<h){
             int m = l + (h-l)/2;
-            int c = nele(a,m);
+            int c = lesst(a,m);
             System.out.println(m+"  "+c);
-            if(c>=d){
+            if(c>=k){
                 h = m;
             }
             else l = m + 1;
@@ -14,17 +13,18 @@ class Solution {
         return l;
     }
 
-    public int nele(int[][] a, int x){
+
+    public int lesst(int[][] a, int n){
+        int r = 0, c = a[0].length-1;
         int cnt = 0;
-        int r = 0;
-        int c = a[0].length-1;
         while(r<a.length&&c>=0){
-            if(a[r][c]<=x){
-                cnt += c+1;
+            if(a[r][c]<=n){
+                cnt += c + 1;
                 r++;
             }
             else c--;
         }
+        System.out.println(cnt);
         return cnt;
     }
 }
