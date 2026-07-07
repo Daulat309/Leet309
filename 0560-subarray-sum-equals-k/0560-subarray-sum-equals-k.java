d9b000b80678fd2d4970ef1a9d24fb7efc4a5b0f
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-07 14:06:29 +0530

    Time: 24 ms (76.99%), Space: 48.5 MB (78.75%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,22 +1,15 @@
 class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int s = 0, cnt = 0;
+    public int subarraySum(int[] a, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0,1);
-        for(int i = 0;i<nums.length;i++){
-            s += nums[i];
-            int m = s - k;
-            int f = map.getOrDefault(m,0);
-            cnt += f;
-            map.put(s,map.getOrDefault(s, 0)+1);
+        int res = 0;
+        int s = 0;
+        for(int i = 0;i<a.length;i++){
+            s += a[i];
+            int f = map.getOrDefault(s-k,0);
+            res += f;
+            map.put(s,map.getOrDefault(s,0)+1);
         }
-        return cnt;
+        return res;
     }
 }
-
-
-
-/*
-here m is which part we can delete from current frame of s
-
-*/
