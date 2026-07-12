8e46352cd2318dd38382eee0240482b559decf11
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-12 10:52:42 +0530

    Time: 29 ms (84.49%), Space: 77 MB (25.63%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,14 +1,21 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        Set<Integer> set = new TreeSet<>();
-        for(int i = 0;i<arr.length;i++) set.add(arr[i]);
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
         HashMap<Integer, Integer> map = new HashMap<>();
-        int k = 1;
-        for(Integer i : set){
-            map.put(i,k);
-            k++;
+        int rank = 1;
+
+        for (int num : sorted) {
+            if (!map.containsKey(num)) {
+                map.put(num, rank++);
+            }
         }
-        for(int i = 0;i<arr.length;i++) arr[i] = map.get(arr[i]);
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = map.get(arr[i]);
+        }
+
         return arr;
     }
 }
