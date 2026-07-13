46dd6f444ee97ad361d8c76fe3b2f7f048c2bf62
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-13 19:29:05 +0530

    Time: 7 ms (27.23%), Space: 43.7 MB (22.13%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,19 +1,18 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
         HashMap<Character, Integer> map = new HashMap<>();
-        int mn = Integer.MAX_VALUE;
         for(int i = 0;i<text.length();i++){
             char c = text.charAt(i);
             if(c=='b'||c=='a'||c=='l'||c=='o'||c=='n'){
                 map.put(c,map.getOrDefault(c,0)+1);
             }
+           
         }
-        if(map.size()!=5) return 0;
-        String s= "balon";
-        for(int i = 0;i<s.length();i++){
-            char c = s.charAt(i);
-            if(c=='l'||c=='o') mn = Math.min(map.get(c)/2,mn);
-            else mn = Math.min(map.get(c),mn);
+        int mn = Integer.MAX_VALUE;
+        if(map.size()<5) return 0;
+        for(Character c : map.keySet()){
+                if(c=='o'||c=='l') mn = Math.min(map.get(c)/2,mn);
+                else mn = Math.min(map.get(c),mn);
         }
         return mn;
     }
