25b7586cacf9c2f1e2ffe6d9296692674f5b75a1
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-13 20:08:26 +0530

    Time: 18 ms (13.32%), Space: 47.4 MB (81.77%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,36 +1,24 @@
-// class Pair{
-//     int f;
-//     int s;
-//     Pair(int a, int b){
-//         f = a;
-//         s = b;
-//     }
-// }
-
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            int n = nums[i];
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
         Queue<Integer> q = new PriorityQueue<>(
             (a,b) -> {
-                return map.get(a) - map.get(b);
+                if(map.get(a).equals(map.get(b))) return a-b;
+                return Integer.compare(map.get(a), map.get(b));
             }
         );
 
-        for(int i = 0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-
-        for(Integer key : map.keySet()){
-            q.offer(key);
-            if(q.size()>k){
-                q.poll();
-            }
+        for(Integer i : map.keySet()){
+            q.offer(i);
+            if(q.size()>k) q.poll();
         }
         int[] a = new int[k];
-        int i = 0;
-        while(!q.isEmpty()){
+        for(int i = a.length-1;i>=0;i--){
             a[i] = q.poll();
-            i++;
         }
         return a;
     }
