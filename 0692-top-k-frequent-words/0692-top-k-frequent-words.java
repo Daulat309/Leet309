df4e541f653f36e27361950a9d134acb342bb517
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-14 18:43:43 +0530

    Time: 8 ms (57.13%), Space: 46.5 MB (68.81%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,25 +1,29 @@
 class Solution {
-    public List<String> topKFrequent(String[] w, int k) {
+    public List<String> topKFrequent(String[] words, int k) {
         HashMap<String, Integer> map = new HashMap<>();
-        Queue<String> q = new PriorityQueue<>(
-            (a,b) ->{
-                if (map.get(a).equals(map.get(b))) return b.compareTo(a);
-                return map.get(a)-map.get(b);
-            }
-        );
-
-        for(String s : w){
+        for(int i = 0;i<words.length; i++){
+            String s = words[i];
             map.put(s,map.getOrDefault(s,0)+1);
         }
 
-        for(String s : map.keySet()){
-            q.offer(s);
-            if(q.size()>k)q.poll();
+        Queue<String> q = new PriorityQueue<>(
+    (a, b) -> {
+        if (map.get(a).equals(map.get(b))) {
+            return b.compareTo(a);
         }
-        List<String> list = new ArrayList<>();
+        return Integer.compare(map.get(a), map.get(b));
+    }
+);
+
+        for(String key : map.keySet()){
+            q.offer(key);
+            if(q.size()>k) q.poll();
+        }
+        ArrayList<String> list = new ArrayList<>();
         while(!q.isEmpty()){
-            list.add(0,q.poll());
+            list.add(q.poll());
         }
+        Collections.reverse(list);
         return list;
     }
 }
