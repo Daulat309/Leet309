f476e5623a0c38a9a13dc35451164830f2eb1e08
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-15 07:27:49 +0530

    Time: 4 ms (19.04%), Space: 46.7 MB (45.99%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,29 +1,29 @@
 class Solution {
-    public int[] kWeakestRows(int[][] mat, int k) {
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> {
-                if (a[0] == b[0]) return b[1] - a[1];
-                return b[0] - a[0];
+    public int[] kWeakestRows(int[][] mat, int k) {
+        for(int i = 0;i<mat.length;i++){
+            for(int j = 1;j<mat[i].length;j++){
+                mat[i][0] += mat[i][j];
+            }
+        }
+
+        Queue<Integer> q = new PriorityQueue<>(
+            (a,b) -> {
+                if(mat[a][0]==mat[b][0]) return Integer.compare(b,a);
+                return mat[b][0] - mat[a][0];
             }
         );
 
-        for (int i = 0; i < mat.length; i++) {
-
-            int soldiers = 0;
-            for (int val : mat[i]) soldiers += val;
-
-            pq.offer(new int[]{soldiers, i});
-
-            if (pq.size() > k) pq.poll();
+        for(int i = 0;i<mat.length;i++){
+            q.offer(i);
+            if(q.size()>k) q.poll();
         }
-
-        int[] ans = new int[k];
-
-        for (int i = k - 1; i >= 0; i--) {
-            ans[i] = pq.poll()[1];
+        int[] a = new int[q.size()];
+        int i = a.length-1;
+        while(!q.isEmpty()){
+            a[i] = q.poll();
+            i--;
         }
-
-        return ans;
+        return a;
     }
 }
