8414f8cdea3a8b06f94ce1d1024bbfa880a5bc08
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-15 09:59:23 +0530

    Time: 16 ms (59.94%), Space: 50.9 MB (69.67%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,30 +1,12 @@
 class Solution {
-    public int kthSmallest(int[][] a, int k) {
-        int l = a[0][0], h = a[a.length-1][a[0].length-1];
-        while(l<h){
-            int m = l + (h-l)/2;
-            int c = lesst(a,m);
-            System.out.println(m+"  "+c);
-            if(c>=k){
-                h = m;
-            }
-            else l = m + 1;
+    public int kthSmallest(int[][] matrix, int k) {
+        Queue<Integer> q = new PriorityQueue<>(
+            (a,b) -> {return b - a;}
+        );
+        for(int[] a : matrix) for(int i : a){
+            q.offer(i);
+            if(q.size()>k) q.poll();
         }
-        return l;
-    }
-
-
-    public int lesst(int[][] a, int n){
-        int r = 0, c = a[0].length-1;
-        int cnt = 0;
-        while(r<a.length&&c>=0){
-            if(a[r][c]<=n){
-                cnt += c + 1;
-                r++;
-            }
-            else c--;
-        }
-        System.out.println(cnt);
-        return cnt;
+        return q.poll();
     }
 }
