94e93c530245c614d907e44b119336253842167b
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-20 08:39:32 +0530

    Time: 7 ms (43.22%), Space: 47.1 MB (64.37%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -2,31 +2,13 @@ class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         k = k % (grid.length*grid[0].length);
         int[] temp = new int[grid.length*grid[0].length];
-        // int mul = grid[0].length;
-        // for(int i = 0;i<grid.length;i++){
-        //     for(int j = 0;j<grid[0].length;j++){
-        //         temp[(i*mul)+j] = grid[i][j];
-        //     }
-        // }
         int s = 0, e = grid.length*grid[0].length-1;
         if(k>0){
-        flip(grid,s,e);
-        flip(grid,s,k-1);
-        flip(grid,k,e);
+            flip(grid,s,e);
+            flip(grid,s,k-1);
+            flip(grid,k,e);
         }
-        // if(k>0){
-        // flip(temp,0,temp.length-1);
-        // flip(temp,0,k-1);
-        // flip(temp,k,temp.length-1);
-        // }
         List<List<Integer>> list = new ArrayList<>();
-        // for(int i = 0;i<grid.length;i++){
-        //     List<Integer> ls = new ArrayList<>();
-        //     for(int j = 0;j<grid[0].length;j++){
-        //         ls.add(grid[i][j]);
-        //     }
-        //     list.add(ls);
-        // }
         for(int i = 0;i<grid.length;i++){
             List<Integer> ls = new ArrayList<>();
             for(int j = 0;j<grid[0].length;j++){
@@ -39,24 +21,11 @@ class Solution {
 
     public void flip(int[][] a, int s, int e){
         while(s<e){
-            int fr = s / a[0].length;
-            int fc = s % a[0].length;
-            int sr = e / a[0].length;
-            int sc = e % a[0].length;
-            int tmp = a[fr][fc];
-            a[fr][fc] = a[sr][sc];
-            a[sr][sc] = tmp;
+            int tmp = a[s / a[0].length][s % a[0].length];
+            a[s / a[0].length][s % a[0].length] = a[e / a[0].length][e % a[0].length];
+            a[e / a[0].length][e % a[0].length] = tmp;
             s++;
             e--;
         }
     }
-    // public void flip(int[] a, int s, int e){
-    //     while(s<e){
-    //         int temp = a[s];
-    //         a[s] = a[e];
-    //         a[e] = temp;
-    //         s++;
-    //         e--;
-    //     }
-    // }
 }
