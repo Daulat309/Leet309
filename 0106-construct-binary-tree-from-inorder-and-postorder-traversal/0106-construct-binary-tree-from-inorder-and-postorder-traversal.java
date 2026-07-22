34b2f19a65b06219da87596ed0635ea21f0478ec
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-22 22:27:00 +0530

    Time: 2 ms (65.36%), Space: 46.5 MB (35.35%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,21 +15,22 @@
  */
 class Solution {
     int idx;
-    HashMap<Integer,Integer> map = new HashMap<>();
-    public TreeNode buildTree(int[] in, int[] post) {
-        idx = post.length-1;
-        for(int i = 0;i<in.length;i++) map.put(in[i],i);
-        return make(post,0,in.length-1);
+    HashMap<Integer, Integer> mp = new HashMap<>();
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        idx = inorder.length-1;
+        
+        for(int i = 0;i<inorder.length;i++) mp.put(inorder[i], i);
+        return build(postorder, 0, inorder.length-1);
     }
 
-    public TreeNode make(int[] post, int l, int h){
+    public TreeNode build(int[] p, int l, int h){
         if(l>h) return null;
-        TreeNode node = new TreeNode(post[idx]);
+        TreeNode node = new TreeNode(p[idx]);
         idx--;
-        int id = map.get(node.val);
-        node.right = make(post,id+1,h);
-        node.left = make(post,l,id-1);
-        
+        int id = mp.get(node.val);
+        node.right = build(p, id+1,h);
+        node.left = build(p, l, id - 1);
+
         return node;
     }
 }
