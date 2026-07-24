0be6b37749b3e904896f13c2f540c34d68bd172e
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-24 13:39:33 +0530

    Time: 0 ms (100%), Space: 44.5 MB (73.28%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,24 +1,23 @@
 class Solution {
+    List<String> list = new ArrayList<>();
     public List<String> generateParenthesis(int n) {
-        List<String> list = new ArrayList<String>();
-        System.out.println(list);
-        add(n, 0, 0,new StringBuilder(),list);
+        add(new StringBuilder(), 0,0,n);
         return list;
+        
     }
 
-    public void add(int n, int o, int c, StringBuilder sb, List<String> list){
-        if(o==n&&c==n){
+    public void add(StringBuilder sb, int open, int close, int n){
+        if(open==n&&open==close){
             list.add(sb.toString());
-            return;
         }
-        if(o<n){
+        if(open<n){
             sb.append('(');
-            add(n, o+1, c,sb,list);
+            add(sb,open+1,close,n);
             sb.deleteCharAt(sb.length()-1);
         }
-        if(c<o){
+        if(close<open){
             sb.append(')');
-            add(n, o, c+1,sb,list);
+            add(sb,open,close+1,n);
             sb.deleteCharAt(sb.length()-1);
         }
     }
