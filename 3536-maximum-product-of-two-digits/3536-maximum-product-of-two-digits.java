02fef659bb08ed1730f3d63eaf988ca0d005b29b
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-25 08:47:25 +0530

    Time: 1 ms (100%), Space: 42.5 MB (74.48%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProduct(int n) {
+        int m1 = 0, m2 = 0;
+        while(n!=0){
+            int d = n%10;
+            if(d>m1){
+                m2 = m1;
+                m1 = d;
+            }
+            else m2 =Math.max(m2,d);
+            n /= 10;
+        }
+        return m1*m2;
+    }
+}
