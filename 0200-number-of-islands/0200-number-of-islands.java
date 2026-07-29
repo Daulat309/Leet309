5e77b8f9ac5ca4203f3c0433f04c99088f152be7
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-29 17:35:13 +0530

    Time: 23 ms (5.64%), Space: 51.7 MB (79.22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,24 +2,29 @@ class Solution {
     int[][] dir = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
     public int numIslands(char[][] grid) {
         int cnt = 0;
+
         for(int i = 0;i<grid.length;i++){
             for(int j = 0;j<grid[0].length;j++){
                 if(grid[i][j]=='1'){
-                    trave(grid, i, j);
+                    System.out.println("Entered");
+                    grid[i][j] = '0';
                     cnt++;
+                    Stack<int[]> st = new Stack<>();
+                    st.push(new int[]{i,j});
+                    while(!st.isEmpty()){
+                        int[] a = st.pop();
+                        for(int[] d : dir){
+                            int ni = a[0]+d[0];
+                            int nj = a[1]+d[1];
+                            if(ni>=0&&nj>=0&&ni<grid.length&&nj<grid[0].length&&grid[ni][nj]=='1'){
+                                grid[ni][nj] = '0';
+                                st.push(new int[]{ni,nj});
+                            }
+                        }
+                    }
                 }
             }
         }
         return cnt;
-    }
-
-    public void trave(char[][] grid, int i, int j){
-        if(i<0||j<0||i==grid.length||j==grid[0].length||grid[i][j]=='0') return;
-        grid[i][j] = '0';
-        for(int k = 0;k<4;k++){
-            int ni = i + dir[k][0];
-            int nj = j + dir[k][1];
-            trave(grid,ni,nj);
-        }
     }
 }
