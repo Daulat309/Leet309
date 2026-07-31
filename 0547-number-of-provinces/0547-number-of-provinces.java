da22094d5c14935651542e5a561cc8ae8d3c1154
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-31 08:48:18 +0530

    Time: 10 ms (5.32%), Space: 47.1 MB (75.1%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,31 +1,32 @@
 class Solution {
     public int findCircleNum(int[][] isc) {
         boolean[] vis = new boolean[isc.length];
+        int v = 0;
         int c = 0;
         Queue<Integer> q = new ArrayDeque<>();
-        for(int i = 0;i<isc.length;i++){
-            for(int j = 0;j<isc[0].length;j++){
-                if(isc[i][j]==1){
-                    isc[i][j]=0;
-                    if(!vis[j]){
+        for(int i = 0;i<isc.length;i++){            
+                    if(!vis[i]){
                         vis[i] = true;
                         c++;
-                        q.offer(j);
+                        v++;
+                        System.out.println(v);
+                        if(v==vis.length) return c;
+                        q.offer(i);
                         while(!q.isEmpty()){
                             int k = q.poll();
-                            for(int cl = 0;cl<isc[k].length;cl++){
-                                if(isc[k][cl]==1){
-                                    if(!vis[cl]){
-                                        vis[cl] = true;
-                                        q.offer(cl);
+                            for(int j = 0;j<isc[0].length;j++){
+                                if(isc[k][j]==1){
+                                    if(!vis[j]){
+                                        vis[j] = true;
+                                        v++;
+                                        System.out.println(v);
+                                        if(v==vis.length) return c;
+                                        q.offer(j);
                                     }
-                                    isc[k][cl] = 0;
                                 }
                             }
                         }
                     }
-                }
-            }
         }
         return c;
     }
