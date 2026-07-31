cd6dd9952991b4ef0ece956a4e7356c39812b0ed
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-31 09:55:48 +0530

    Time: 2 ms (89.73%), Space: 43.9 MB (85.38%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -9,39 +9,35 @@ class Pair{
 class Solution {
     int[][] dir = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
     public int orangesRotting(int[][] grid) {
-        //int mx = Math.max(grid.length, grid[0].length);
         int mx = grid.length*grid[0].length;
         int total = 0;
+        Queue<Pair> st = new ArrayDeque<>();
         for(int i = 0;i<grid.length;i++){
             for(int j = 0;j<grid[0].length;j++){
                 if(grid[i][j]==1) total++;
-            }
-        }
-        for(int rot = 0;rot<mx;rot++){
-            if(total==0) return rot;
-            Stack<Pair> st = new Stack<>();
-            for(int i = 0;i<grid.length;i++){
-                for(int j = 0;j<grid[0].length;j++){
-                    if(grid[i][j]==2){
-                        for(int[] d : dir){
-                            int ni = i + d[0];
-                            int nj = j + d[1];
-                            if(ni>=0&&nj>=0&&ni<grid.length&&nj<grid[0].length&&grid[ni][nj]==1){
-                            st.push(new Pair(ni,nj));
-                            }
-                        }
-                        
-                    }
+                if(grid[i][j]==2) {
+                    st.offer(new Pair(i,j));
                 }
             }
-            while(!st.isEmpty()){
-                Pair p = st.pop();
-                if(grid[p.i][p.j]==1){
-                    grid[p.i][p.j]=2;
+        }
+        if(total==0) return 0;
+        int min = 0;
+        while(!st.isEmpty()&&total>0){
+            int l = st.size();
+            while(l-->0){
+            Pair p = st.poll();
+            for(int[] d : dir){
+                int ni = p.i + d[0];
+                int nj = p.j + d[1];
+                if(ni>=0&&nj>=0&&ni<grid.length&&nj<grid[0].length&&grid[ni][nj]==1){
                     total--;
+                    grid[ni][nj]=2;
+                    st.offer(new Pair(ni,nj));
                 }
             }
+            }
+            min++;
         }
-        return total==0?mx : -1;
+        return total==0?min : -1;
     }
 }
