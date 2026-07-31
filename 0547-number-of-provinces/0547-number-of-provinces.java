8d974f8c1e1563554134e9091639509333ffa323
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-07-31 08:50:13 +0530

    Time: 2 ms (41.74%), Space: 47.5 MB (25.28%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,16 +1,12 @@
 class Solution {
     public int findCircleNum(int[][] isc) {
         boolean[] vis = new boolean[isc.length];
-        int v = 0;
         int c = 0;
         Queue<Integer> q = new ArrayDeque<>();
         for(int i = 0;i<isc.length;i++){            
                     if(!vis[i]){
                         vis[i] = true;
                         c++;
-                        v++;
-                        System.out.println(v);
-                        if(v==vis.length) return c;
                         q.offer(i);
                         while(!q.isEmpty()){
                             int k = q.poll();
@@ -18,9 +14,6 @@ class Solution {
                                 if(isc[k][j]==1){
                                     if(!vis[j]){
                                         vis[j] = true;
-                                        v++;
-                                        System.out.println(v);
-                                        if(v==vis.length) return c;
                                         q.offer(j);
                                     }
                                 }
