c75e84b7e7e11a1817e9f566a3fd835f8887a20b
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-01 10:05:03 +0530

    Time: 0 ms (100%), Space: 47.4 MB (7.08%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,30 +1,24 @@
 class Solution {
-    boolean res = true;
     public boolean isBipartite(int[][] graph) {
         int[] col = new int[graph.length];
-        Arrays.fill(col, -1);
+        Arrays.fill(col,-1);
         for(int i = 0;i<graph.length;i++){
             if(col[i]==-1){
                 col[i] = 1;
-                dfs(graph,i,col);
-                if(!res) return false;
+                if(!dfs(graph,i,col)) return false;
             }
         }
         return true;
     }
 
-    public void dfs(int[][] grid, int i, int[] col){
-                    for(int k : grid[i]){
-                        if(col[k]!=-1&&col[k]==col[i]){
-                            res = false;
-                            return;
-                        }
-                        
-                        if(col[k]==-1){
-                            if(col[i]==1) col[k] = 2;
-                            else col[k] = 1;
-                            dfs(grid,k,col);
-                        }
-                    }
+    public boolean dfs(int[][] graph, int i, int[] col ){
+        for(int k : graph[i]){
+            if(col[k]!=-1&&col[k]==col[i]) return false;
+            else if(col[k]==-1){
+                col[k] = 3 - col[i];
+                if(!dfs(graph,k,col)) return false;
+            }
+        }
+        return true;
     }
 }
