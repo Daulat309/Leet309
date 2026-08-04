052fc8e33b945b6a1e9ab08c44761d8b8bdbbde8
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-04 11:42:15 +0530

    Time: 100 ms (5.07%), Space: 46.4 MB (62.6%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -0,0 +1,51 @@
+class Pair{
+    int i;
+    int j;
+    int x;
+    Pair(int i, int j, int x){
+        this.i = i;
+        this.j = j;
+        this.x = x;
+    }
+}
+class Solution {
+    public int swimInWater(int[][] grid) {
+        int n = grid.length-1;
+        int m = grid[0].length-1;
+        int[] dx = {0,1,0,-1};
+        int[] dy = {1,0,-1,0};
+        boolean[][] vis = new boolean[n+1][m+1];
+        int mn = Integer.MAX_VALUE;
+        Queue<Pair> q = new PriorityQueue<>(
+            (a,b) -> {return Integer.compare(a.x,b.x);}
+        );
+        //Queue<Pair> q = new ArrayDeque<>();
+        q.offer(new Pair(0,0,grid[0][0]));
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            int i = p.i;
+            int j = p.j;
+            int x = p.x;
+
+            if(i==n&&j==m){
+                System.out.println("Entered!");
+                mn = Math.min(x,mn);
+                continue;
+            }
+            if(vis[i][j]){
+                continue;
+            }
+
+            for(int k = 0;k<4;k++){
+                int ni = i + dx[k];
+                int nj = j + dy[k];
+                if(ni>=0&&nj>=0&&ni<=n&&nj<=m&&!vis[ni][nj]){
+                    System.out.println("Inserted");
+                    q.offer(new Pair(ni,nj,Math.max(x,grid[ni][nj])));
+                }
+            }
+            vis[i][j] = true;
+        }
+        return mn;
+    }
+}
