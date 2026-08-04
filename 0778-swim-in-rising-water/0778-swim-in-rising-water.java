7a0bd716b689e974dda2212fb53b034e75811daf
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-04 13:07:15 +0530

    Time: 9 ms (86.74%), Space: 46.4 MB (62.6%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -1,96 +1,50 @@
-class Pair{
-    int i;
-    int j;
-    int x;
-    Pair(int i, int j, int x){
+class Pair {
+    int i, j, t;
+
+    Pair(int i, int j, int t) {
         this.i = i;
         this.j = j;
-        this.x = x;
+        this.t = t;
     }
 }
+
 class Solution {
-    int[] dx = {0,1,0,-1};
-    int[] dy = {1,0,-1,0};
-    // public int swimInWater(int[][] grid) {
-    //     int n = grid.length-1;
-    //     int m = grid[0].length-1;
+    private static final int[] dx = {0, 1, 0, -1};
+    private static final int[] dy = {1, 0, -1, 0};
 
-    //     boolean[][] vis = new boolean[n+1][m+1];
-    //     Queue<Pair> q = new PriorityQueue<>(
-    //         (a,b) -> {return Integer.compare(a.x,b.x);}
-    //     );
-    //     q.offer(new Pair(0,0,grid[0][0]));
-    //     vis[0][0] = true;
-    //     while(!q.isEmpty()){
-    //         Pair p = q.poll();
-    //         int i = p.i;
-    //         int j = p.j;
-    //         int x = p.x;
-    //         if(i==n&&j==m){
-    //             return x;
-    //         }
-    //         for(int k = 0;k<4;k++){
-    //             int ni = i + dx[k];
-    //             int nj = j + dy[k];
-    //             if(ni>=0&&nj>=0&&ni<=n&&nj<=m&&!vis[ni][nj]){
-    //                 vis[i][j] = true;
-    //                 q.offer(new Pair(ni,nj,Math.max(x,grid[ni][nj])));
-    //             }
-    //         }
-    //     }
-    //     return mn;
-    // }
     public int swimInWater(int[][] grid) {
-        int n = grid.length-1;
-        int m = grid[0].length-1;
-        int l = grid[0][0];
-        int h = 0;
-        for(int i = 0;i<=n;i++){
-            for(int j = 0;j<=m;j++){
-                h = Math.max(grid[i][j],h);
+        int n = grid.length;
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+            (a, b) -> Integer.compare(a.t, b.t)
+        );
+
+        boolean[][] vis = new boolean[n][n];
+
+        pq.offer(new Pair(0, 0, grid[0][0]));
+        vis[0][0] = true;
+
+        while (!pq.isEmpty()) {
+            Pair cur = pq.poll();
+
+            if (cur.i == n - 1 && cur.j == n - 1)
+                return cur.t;
+
+            for (int k = 0; k < 4; k++) {
+                int ni = cur.i + dx[k];
+                int nj = cur.j + dy[k];
+
+                if (ni >= 0 && nj >= 0 && ni < n && nj < n && !vis[ni][nj]) {
+                    vis[ni][nj] = true;
+                    pq.offer(new Pair(
+                        ni,
+                        nj,
+                        Math.max(cur.t, grid[ni][nj])
+                    ));
+                }
             }
         }
-        while(l<h){
-            int md  = l + (h-l)/2;
-            if(bfs(grid,0,0,md)) h = md;
-            else l = md + 1;
-        }
-        return l;
-    }
 
-
-    public boolean bfs(int[][] a, int si, int sj, int x) {
-    int n = a.length;
-    int m = a[0].length;
-
-    boolean[][] vis = new boolean[n][m];
-    Queue<int[]> q = new LinkedList<>();
-
-    q.offer(new int[]{si, sj});
-    vis[si][sj] = true;
-
-    while (!q.isEmpty()) {
-        int[] cur = q.poll();
-        int i = cur[0];
-        int j = cur[1];
-
-        if (i == n - 1 && j == m - 1) {
-            return true;
-        }
-
-        for (int k = 0; k < 4; k++) {
-            int ni = i + dx[k];
-            int nj = j + dy[k];
-
-            if (ni >= 0 && nj >= 0 && ni < n && nj < m &&
-                !vis[ni][nj] && a[ni][nj] <= x) {
-
-                vis[ni][nj] = true;
-                q.offer(new int[]{ni, nj});
-            }
-        }
-    }
-
-    return false;
+        return -1;
     }
 }
