a219e6898b99e3d636bb1cdeea68e4a68f721113
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-05 10:40:13 +0530

    Time: 4 ms (99.64%), Space: 46.9 MB (16.68%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,23 +1,27 @@
 class Solution {
-    public int findCheapestPrice(int n, int[][] edges, int src, int dst, int k) {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         int INF = (int)1e8;
         int[] dist = new int[n];
         Arrays.fill(dist, INF);
-        int[] temp = new int[n];
-        Arrays.fill(temp, INF);
-        dist[src] = 0; 
+        dist[src] = 0;
 
-        for(int i = 0; i <= k; i++){
-            for(int[] e : edges){
-                int s = e[0], d = e[1], x = e[2];
-                if(dist[s] != INF && dist[s] + x < temp[d]){
-                    temp[d] = dist[s] + x;
+        for (int i = 0; i <= k; i++) {
+            int[] temp = dist.clone();
+            boolean updated = false;
+
+            for (int[] f : flights) {
+                int u = f[0], v = f[1], w = f[2];
+
+                if (dist[u] != INF && dist[u] + w < temp[v]) {
+                    temp[v] = dist[u] + w;
+                    updated = true;
                 }
             }
-            for(int m = 0;m<n;m++){
-                dist[m] = temp[m];
-            }
+
+            dist = temp;
+            if (!updated) break;
         }
-        return dist[dst]==INF? -1 : dist[dst];
+
+        return dist[dst] == INF ? -1 : dist[dst];
     }
 }
