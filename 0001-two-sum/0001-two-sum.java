e1409f27432b8fb0b2020ce263746dc730f45b31
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-06 07:52:13 +0530

    Time: 8 ms (46.59%), Space: 46.7 MB (90.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,46 +1,36 @@
-class Solution {
-    public int[] twoSum(int[] nums, int x) {
-        int[] a = new int[nums.length];
-        int p = -1;
-        for(int k : nums) a[++p] = k;
-        Arrays.sort(nums);
-        p = -1;
-        int q = (int)1e8, l = 0, h = nums.length-1;
-        while(l<h){
-            int s = nums[l]+nums[h];
-            if(s==x){
-                p = nums[l];
-                q = nums[h];
-                break;
-            }
-            if(s>x) h--;
-            else l++;
-        }
-        if(q==(int)1e8) return new int[]{-1,-1};
-        int m = -1, n = -1;
-        for(int i = 0;i<a.length;i++){
-            if(a[i]==p||a[i]==q){
-                if(a[i]==p&&m==-1){
-                    m = i;
-                }
-                else if(a[i]==q&&n==-1){
-                    n = i;
-                }
-            }
-            if(m!=-1&&n!=-1) return new int[]{m,n};
-        }
-        return new int[]{-1,-1};
-    }
+class Pair {
+    int val, idx;
 
+    Pair(int val, int idx) {
+        this.val = val;
+        this.idx = idx;
+    }
 }
 
-// class Solution {
-//     public int[] twoSum(int[] nums, int x) {
-//         HashMap<Integer, Integer> set = new HashMap<>();
-//         for(int i = 0;i<nums.length;i++){
-//             if(set.containsKey(x-nums[i])) return new int[]{set.get(x-nums[i]), i};
-//             set.put(nums[i], i);
-//         }
-//         return new int[] {-1,-1};
-//     }
-// }
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Pair[] arr = new Pair[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = new Pair(nums[i], i);
+        }
+
+        Arrays.sort(arr, (a, b) -> Integer.compare(a.val, b.val));
+
+        int l = 0, r = arr.length - 1;
+
+        while (l < r) {
+            int sum = arr[l].val + arr[r].val;
+
+            if (sum == target) {
+                return new int[]{arr[l].idx, arr[r].idx};
+            } else if (sum < target) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+}
