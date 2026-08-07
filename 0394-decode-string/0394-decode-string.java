e11b828d0f94c654da257144b3af5a2435d6717a
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-07 07:53:05 +0530

    Time: 1 ms (85.27%), Space: 43 MB (39.99%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,61 +1,39 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Character> st = new Stack<>();
-        int i = 0;
-        int open = 0;
-        StringBuilder msb = new StringBuilder();
-        while(i<s.length()){
-            if(s.charAt(i)==']'){
-                StringBuilder sb = new StringBuilder();
-                while(st.peek()!='['){
-                    sb.append(st.pop());
-                }
-                st.pop();
-                open--;
-                sb.reverse();
-                int k = 0;
-                int mul = 1;
-                while(!st.isEmpty()&&st.peek()>='0'&&st.peek()<='9'){
-                    k = (st.pop()-'0')*mul + k;
-                    mul *= 10;
-                }
-                if(open!=0){
-                    while(k-->0){
-                        for(int j = 0;j<sb.length();j++){
-                            st.push(sb.charAt(j));
-                        }
-                    }
-                    sb.setLength(0);
-                }
-                else{
-                    StringBuilder nsb = new StringBuilder();
-                    while(!st.isEmpty()){
-                        nsb.append(st.pop());
-                    }
-                    nsb.reverse();
-                    for(int j = 0;j<nsb.length();j++){
-                        msb.append(nsb.charAt(j));
-                    }
-                    while(k-->0){
-                        for(int j = 0;j<sb.length();j++){
-                            msb.append(sb.charAt(j));
-                        }
-                    }
-                    sb.setLength(0);
-                }
-                
-            }
-            else{
-                st.push(s.charAt(i));
-                if(s.charAt(i)=='[') open++;
-            }
-            i++;
-        }
-        StringBuilder sb = new StringBuilder();
-        while(!st.isEmpty()){
-            sb.append(st.pop());
-        }
-        msb.append(sb.reverse().toString());
-        return msb.toString();
+        Stack<Integer> countStack = new Stack<>();
+Stack<StringBuilder> stringStack = new Stack<>();
+
+StringBuilder curr = new StringBuilder();
+int num = 0;
+
+for(char ch : s.toCharArray()){
+
+    if(Character.isDigit(ch)){
+        num = num*10 + (ch-'0');
+    }
+
+    else if(ch=='['){
+        countStack.push(num);
+        stringStack.push(curr);
+        curr = new StringBuilder();
+        num = 0;
+    }
+
+    else if(ch==']'){
+        int k = countStack.pop();
+        StringBuilder prev = stringStack.pop();
+
+        while(k-- > 0)
+            prev.append(curr);
+
+        curr = prev;
+    }
+
+    else{
+        curr.append(ch);
+    }
+}
+
+return curr.toString();
     }
 }
