d85631c811c40c9306db1584336edae2df35a829
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-13 19:06:54 +0530

    Time: 3 ms (17.46%), Space: 46.6 MB (14.21%) - LeetHub

--- 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
+++ 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
@@ -1,19 +1,17 @@
 class Solution {
     int[] segT;
     int n;
-    int cur = -1;
     public int numOfUnplacedFruits(int[] f, int[] b) {
         int cnt = 0;
         n = b.length;
         segT = new int[4*n];
         buildT(0, 0, n-1, b);
         for(int i = 0;i<f.length;i++){
-            get(0,0,n-1,f[i]);
+            int cur = get(0,0,n-1,f[i]);
             if(cur==-1) cnt++;
             else{
                 update(0,cur,0, n-1 );
             }
-            cur = -1;
             
         }
         return cnt;
@@ -33,21 +31,18 @@ class Solution {
         segT[idx] = Math.max(segT[lft], segT[rht]);
     }
 
-    public void get(int idx, int l, int r, int k){
+    public int get(int idx, int l, int r, int k){
         if(l==r){
-            if(segT[idx]>=k){
-                cur = l;
-            }
-            return;
+            return segT[idx] >= k ? l : -1;
         }
         int lft = 2*idx+1;
         int rht = 2*idx+2;
         int md = l + (r-l)/2;
         if(segT[idx]>=k){
-            if(segT[lft]>=k) get(lft, l, md, k);
-            else get(rht, md+1, r, k);
+            if(segT[lft]>=k) return get(lft, l, md, k);
+            return get(rht, md+1, r, k);
         }
-        return;
+        return -1;
     }
 
     public void update(int idx, int i, int l, int r){
