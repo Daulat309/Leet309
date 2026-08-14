bc3a344c08a09ef128d3c82df0ce7451b3161e7a
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-14 11:23:31 +0530

    Time: 0 ms (100%), Space: 41.9 MB (73.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,14 @@
 class Solution {
-    int cnt = 0;
     public int climbStairs(int n) {
+        if(n<3) return n;
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return cli(n, dp);
-    }
-
-    public int cli(int n, int[] dp){
-        if(n<0) return 0;
-        if(n==0) return 1;
-        if(dp[n]!=-1) return dp[n];
-        dp[n] = cli(n-1,dp) + cli(n-2,dp);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         return dp[n];
     }
 }
