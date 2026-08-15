413b4686b4956e8d110d9aa7d50ade507a259b40
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-15 15:42:59 +0530

    Time: 0 ms (100%), Space: 43 MB (5.71%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return ro(0,n,dp,nums);
-    }
-
-    public int ro(int i,int n,int[] dp, int[] nums){
-        if(i>=n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        dp[i] = Math.max(nums[i]+ro(i+2,n,dp,nums), ro(i+1, n, dp, nums));
-        return dp[i];
+        int[] r = new int[n];
+        r[0] = nums[0];
+        if(n==1) return r[0];
+        r[1] = Math.max(r[0], nums[1]);
+        for(int i = 2;i<n; i++){
+            int steal = nums[i] + r[i-2];
+            int sk = r[i-1];
+            r[i] = Math.max(steal, sk);
+        }
+        return r[n-1];
     }
 }
