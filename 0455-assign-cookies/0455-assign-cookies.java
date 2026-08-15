0094043b6d4944981b6b21991ca89c79c6a383a3
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-15 14:12:56 +0530

    Time: 14 ms (91.84%), Space: 51.4 MB (56.24%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -3,14 +3,12 @@ class Solution {
         Arrays.sort(s);
         Arrays.sort(g);
         int h = 0, l = 0;
-        int c = 0;
         while(h<s.length&&l<g.length){
             if(s[h]>=g[l]){
-                c++;
                 l++;
             }
             h++;
         }
-        return c;
+        return l;
     }
 }
