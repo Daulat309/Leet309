c2c42ff13ef3a0d47703ea26249e2ce68e327300
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-15 14:39:10 +0530

    Time: 2 ms (88.92%), Space: 48 MB (43.49%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        if(nums.length==1) return true;
         int cur = 0;
         for(int i = 0;i<nums.length-1;i++){
             cur = Math.max(cur, i + nums[i]);
