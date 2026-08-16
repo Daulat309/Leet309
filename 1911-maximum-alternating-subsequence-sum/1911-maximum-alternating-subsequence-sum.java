a44072d5da2b423689dc4b73d90caa66c4d20850
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-16 23:35:30 +0530

    Time: 6 ms (95.43%), Space: 98.1 MB (83.57%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
@@ -1,27 +1,16 @@
 class Solution {
-    long[][] dp = new long[100001][2];
     public long maxAlternatingSum(int[] nums) {
-        for(long[] a : dp){
-            Arrays.fill(a, -1);
+        long even = 0;
+        long odd = 0;
+
+        for (int num : nums) {
+            long newEven = Math.max(even, odd + num);
+            long newOdd = Math.max(odd, even - num);
+
+            even = newEven;
+            odd = newOdd;
         }
-        return go(0, true, nums);
-    }
 
-    public long go(int idx, boolean flag, int[] nums){
-        if(idx==nums.length) return 0;
-        int m = 0;
-        if(flag) m = 1;
-        if(dp[idx][m]!=-1) return dp[idx][m];
-        long skip = go(idx+1, flag, nums);
-        long val = nums[idx];
-        if(!flag) val *= -1;
-
-        long take = go(idx+1, !flag, nums) + val;
-        
-
-        dp[idx][m] = Math.max(take, skip);
-
-        return dp[idx][m];
-        
+        return even;
     }
 }
