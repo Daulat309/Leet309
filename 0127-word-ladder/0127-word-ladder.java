3bd899759c6d51cca3da325fdc2ea57c6ea62b30
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-17 19:09:14 +0530

    Time: 75 ms (59.48%), Space: 47.8 MB (74.66%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,45 +1,56 @@
-class Pair{
+class Pair {
     String ss;
     int cc;
 
-    Pair(String ss, int cc){
+    Pair(String ss, int cc) {
         this.ss = ss;
         this.cc = cc;
     }
 }
+
 class Solution {
     public int ladderLength(String bw, String ew, List<String> wl) {
-        HashMap<String, Integer> mp = new HashMap<>();
-        for(String s : wl){
-            mp.put(s,1);
-        }
-        mp.put(bw, 1);
-        Queue<Pair> q = new ArrayDeque<>();
-        if(!mp.containsKey(ew)) return 0;
-        q.offer(new Pair(bw, 1));
-        mp.remove(bw);
 
-        while(!q.isEmpty()){
+        HashSet<String> set = new HashSet<>(wl);
+
+        if (!set.contains(ew)) return 0;
+
+        Queue<Pair> q = new ArrayDeque<>();
+        q.offer(new Pair(bw, 1));
+        set.remove(bw);
+
+        while (!q.isEmpty()) {
 
             Pair p = q.poll();
             String s = p.ss;
             int ct = p.cc;
+
             char[] arr = s.toCharArray();
-            for(int i  = 0;i<s.length();i++){
+
+            for (int i = 0; i < s.length(); i++) {
+
                 char ch = arr[i];
-                for(char c = 'a';c<='z';c++){
-                    if(arr[i]==c) continue;
-                    arr[i] = (char)c;
+
+                for (char c = 'a'; c <= 'z'; c++) {
+
+                    if (ch == c) continue;
+
+                    arr[i] = c;
                     String str = new String(arr);
-                    if(str.equals(ew)) return ct+1;
-                    if(mp.containsKey(str)){
+
+                    if (str.equals(ew))
+                        return ct + 1;
+
+                    if (set.contains(str)) {
                         q.offer(new Pair(str, ct + 1));
-                        mp.remove(str);
+                        set.remove(str);
                     }
                 }
-                arr[i] = (char)ch;
+
+                arr[i] = ch;
             }
         }
+
         return 0;
     }
 }
