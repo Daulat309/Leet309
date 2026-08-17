10e0ad810d69d7094d269cdac293b69a5221f643
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-17 08:48:31 +0530

    Time: 1 ms (100%), Space: 49 MB (15.85%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,61 +1,45 @@
 class Solution {
     public double findMedianSortedArrays(int[] a, int[] b) {
-        int m = a.length, n = b.length, l = m + n;
-        int md = l/2;
-        if(m==0){
-            if(l==1) return (double)b[0];
-            if((l&1)!=1){
-                return ((double)b[md]+(double)b[md-1])/2.0;
-            }
-            return (double)b[md];
+        int m = a.length, n = b.length;
+        int total = m + n;
+        int mid = total / 2;
+
+        if (m == 0) {
+            return total % 2 == 0
+                    ? ((double) b[mid - 1] + b[mid]) / 2
+                    : b[mid];
         }
-        if(n==0){
-            if(l==1) return (double)a[0];
-            if((l&1)!=1){
-                return ((double)a[md]+(double)a[md-1])/2.0;
-            }
-            return (double)a[md];
+
+        if (n == 0) {
+            return total % 2 == 0
+                    ? ((double) a[mid - 1] + a[mid]) / 2
+                    : a[mid];
         }
+
         int x = 0, y = n;
-        while(x+y>md){
-            y--;
-        }
-        while(x+y<md){
+
+        while (x + y > mid) y--;
+        while (x + y < mid) x++;
+
+        while (true) {
+            int left = Integer.MIN_VALUE;
+            int right = Integer.MAX_VALUE;
+
+            if (x > 0) left = Math.max(left, a[x - 1]);
+            if (y > 0) left = Math.max(left, b[y - 1]);
+
+            if (x < m) right = Math.min(right, a[x]);
+            if (y < n) right = Math.min(right, b[y]);
+
+            if (left <= right) {
+                if (total % 2 == 0)
+                    return ((double) left + right) / 2.0;
+
+                return right;
+            }
+
             x++;
-        }
-        
-        
-
-        int fmm = Integer.MIN_VALUE;
-        int smn = Integer.MAX_VALUE;
-
-        if(x != 0) fmm = Math.max(fmm, a[x - 1]);
-        if(y != 0) fmm = Math.max(fmm, b[y - 1]);
-
-        if(x != m) smn = Math.min(smn, a[x]);
-        if(y != n) smn = Math.min(smn, b[y]);
-        
-        
-        while(fmm > smn){ 
-            x++; 
-            y--; 
-
-            fmm = Integer.MIN_VALUE;
-            smn = Integer.MAX_VALUE;
-
-            if(x != 0) fmm = Math.max(fmm, a[x - 1]);
-            if(y != 0) fmm = Math.max(fmm, b[y - 1]);
-
-            if(x != m) smn = Math.min(smn, a[x]);
-            if(y != n) smn = Math.min(smn, b[y]);
-        }
-        System.out.println(x + "  " + y +"  "+fmm +"  "+smn);
-        if((l&1)==0){
-            double res = ((double)fmm+(double)smn)/2.0;
-            return res;
-        }
-        else{
-            return (double)smn;
+            y--;
         }
     }
 }
