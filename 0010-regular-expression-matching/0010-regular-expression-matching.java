eece1b73e13cf35b2bf711b9ea430f095c0a52ba
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-18 12:28:42 +0530

    Time: 1 ms (99.99%), Space: 43.9 MB (15.33%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,48 +1,85 @@
-class Solution { 
-    public boolean isMatch(String s, String p) { 
-        return check(s, p); 
-    } 
+// class Solution { 
+//     public boolean isMatch(String s, String p) { 
+//         return check(s, p); 
+//     } 
  
-    public boolean check(String s, String p) { 
+//     public boolean check(String s, String p) { 
         
-        if(s.length()==0 && p.length()==0) return true;
+//         if(s.length()==0 && p.length()==0) return true;
         
-        if(s.length()==0) { 
-            while(1 < p.length() && p.charAt(1) == '*') {
-                p = p.substring(2);
-            }
-            return p.length()==0;
+//         if(s.length()==0) { 
+//             while(1 < p.length() && p.charAt(1) == '*') {
+//                 p = p.substring(2);
+//             }
+//             return p.length()==0;
+//         }
+        
+//         if(p.length()==0) return false; 
+        
+//         if(1 < p.length() && p.charAt(1) == '*') { 
+            
+//             if(p.charAt(0) == '.') { 
+//                 return check(s.substring(1), p) || check(s, p.substring(2)); 
+//             } 
+//             else { 
+//                 if(s.charAt(0) == p.charAt(0)) { 
+//                     return check(s.substring(1), p) || check(s, p.substring(2)); 
+//                 } 
+//                 else { 
+//                     return check(s, p.substring(2)); 
+//                 } 
+//             } 
+//         } 
+        
+//         else if(p.charAt(0) == '.') { 
+//             return check(s.substring(1), p.substring(1)); 
+//         } 
+        
+//         else { 
+//             if(s.charAt(0) != p.charAt(0)) return false; 
+            
+//             return check(s.substring(1), p.substring(1)); 
+//         } 
+//     } 
+// }
+
+class Solution {
+    Boolean[][] dp;
+
+    public boolean isMatch(String s, String p) {
+        dp = new Boolean[s.length() + 1][p.length() + 1];
+        return check(0, 0, s, p);
+    }
+
+    public boolean check(int i, int j, String s, String p) {
+
+        if (j == p.length()) {
+            return i == s.length();
         }
-        
-        if(p.length()==0) return false; 
-        
-        if(1 < p.length() && p.charAt(1) == '*') { 
-            
-            if(p.charAt(0) == '.') { 
-                return check(s.substring(1), p) || 
-                       check(s, p.substring(2)); 
-            } 
-            else { 
-                if(s.charAt(0) == p.charAt(0)) { 
-                    return check(s.substring(1), p) || 
-                           check(s, p.substring(2)); 
-                } 
-                else { 
-                    return check(s, p.substring(2)); 
-                } 
-            } 
-        } 
-        
-        else if(p.charAt(0) == '.') { 
-            return check(
-                         s.substring(1), 
-                         p.substring(1)); 
-        } 
-        
-        else { 
-            if(s.charAt(0) != p.charAt(0)) return false; 
-            
-            return check(s.substring(1), p.substring(1)); 
-        } 
-    } 
+
+        if (dp[i][j] != null) {
+            return dp[i][j];
+        }
+
+        boolean firstMatch = i < s.length() &&
+                             (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+
+        boolean ans;
+
+        // x*
+        if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+
+            // Option 1: skip x*
+            // Option 2: consume one character
+            ans = check(i, j + 2, s, p) ||
+                  (firstMatch && check(i + 1, j, s, p));
+
+        } else {
+
+            // Normal character or '.'
+            ans = firstMatch && check(i + 1, j + 1, s, p);
+        }
+
+        return dp[i][j] = ans;
+    }
 }
