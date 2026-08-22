4dc5a9f6fcb10270e5b1d68d69a9e244756406df
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-22 08:50:53 +0530

    Time: 3 ms (86.03%), Space: 43.2 MB (57.72%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
-
         Stack<Character> st = new Stack<>();
-
-        for(char c : s.toCharArray()) {
-
-            if(c == '(') st.push(')');
-
-            else if(c == '{') st.push('}');
-
-            else if(c == '[') st.push(']');
-
-            else {
-
-                if(st.isEmpty() || st.pop() != c) {
-                    return false;
-                }
+        int l = 0;
+        while(l<s.length()){
+            char c = s.charAt(l);
+            if(c=='['){
+                st.push(']');
             }
+            else if(c=='{'){
+                st.push('}');
+            }
+            else if(c=='('){
+                st.push(')');
+            }
+            else if(st.isEmpty()||st.peek()!=c) return false;
+            else st.pop();
+            l++;
         }
-
         return st.isEmpty();
     }
 }
