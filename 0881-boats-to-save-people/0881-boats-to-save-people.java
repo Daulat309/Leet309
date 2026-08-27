9d42e1138d5f983ecd6e6d65e4ed64012aab68ec
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-27 21:15:14 +0530

    Time: 20 ms (83.49%), Space: 56.7 MB (20.56%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -2,36 +2,15 @@ class Solution {
     public int numRescueBoats(int[] a, int x) {
         Arrays.sort(a);
         int n = a.length;
-        // int mn = 1, mx = n;
-        // while(mn<=mx){
-        //     int md = mn + (mx-mn)/2;
-        //     if(isp(p,l, md)){
-        //         mx = md - 1;
-        //     }
-        //     else mn = md + 1;
-        // }
-        // return mn;
-
         int l = 0, h = a.length-1;
         int cnt = 0;
         while(l<=h){
             if(a[l]+a[h]<=x){
-                cnt++;
                 l++;
-                h--;
             }
-            else{
-                h--;
-                cnt++;
-            }
+            h--;
+            cnt++;
         }
         return cnt;
     }
-
-
-    // public boolean isp(int[] a, int x, int y){
-        
-    //     if(cnt<=y) return true;
-    //     return false;
-    // }
 }
