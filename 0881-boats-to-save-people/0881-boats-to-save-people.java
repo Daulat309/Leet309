ac2436431a1045df75c4ea9c1967e63dc2a8d9dd
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-08-27 21:10:48 +0530

    Time: 19 ms (98.83%), Space: 56.4 MB (57.15%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,22 +1,17 @@
 class Solution {
-    public int numRescueBoats(int[] p, int l) {
-        Arrays.sort(p);
-        int n = p.length;
-        int mn = 1, mx = n;
-        int res = n;
-        while(mn<=mx){
-            int md = mn + (mx-mn)/2;
-            if(isp(p,l, md)){
-                res = md;
-                mx = md - 1;
-            }
-            else mn = md + 1;
-        }
-        return mn;
-    }
+    public int numRescueBoats(int[] a, int x) {
+        Arrays.sort(a);
+        int n = a.length;
+        // int mn = 1, mx = n;
+        // while(mn<=mx){
+        //     int md = mn + (mx-mn)/2;
+        //     if(isp(p,l, md)){
+        //         mx = md - 1;
+        //     }
+        //     else mn = md + 1;
+        // }
+        // return mn;
 
-
-    public boolean isp(int[] a, int x, int y){
         int l = 0, h = a.length-1;
         int cnt = 0;
         while(l<=h){
@@ -30,7 +25,13 @@ class Solution {
                 cnt++;
             }
         }
-        if(cnt<=y) return true;
-        return false;
+        return cnt;
     }
+
+
+    // public boolean isp(int[] a, int x, int y){
+        
+    //     if(cnt<=y) return true;
+    //     return false;
+    // }
 }
