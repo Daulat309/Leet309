e6f95eb44cbe0806799a5936c3619d93eee3051e
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-09-02 22:42:17 +0530

    Time: 0 ms (100%), Space: 42.5 MB (52.3%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,11 +1,9 @@
 class Solution {
     public int reverseBits(int n) {
         int res = 0;
-        for(int i = 0;i<31;i++){
-            int k = n&1;
-            int l = k<<(31-i);
-            n = n>>1;
-            res = res | l;
+        for(int i = 0;i<32;i++){
+            res = (res<<1) | (n&1);
+            n >>>= 1;
         }
         return res;
     }
