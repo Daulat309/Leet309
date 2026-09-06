ea800b293937fd4c08a247000d5e21b5ed368444
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-09-06 11:34:19 +0530

    Time: 26 ms (9%), Space: 54.6 MB (27.55%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -15,11 +15,13 @@ class Solution {
 
     if(dp[i][j] != -1) return dp[i][j];
 
-    int ans = check(i + 1, j, s, t); // don't choose s[i]
+    int ans = 0; // don't choose s[i]
 
     if(s.charAt(i) == t.charAt(j)) {
-        ans += check(i + 1, j + 1, s, t); // choose s[i]
+        ans += check(i + 1, j + 1, s, t);
+        ans += check(i + 1, j, s, t);
     }
+    else ans += check(i + 1, j , s, t);
 
     return dp[i][j] = ans;
     }
