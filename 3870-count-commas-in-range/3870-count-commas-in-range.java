b4c1874596331fe77493b55e3b14a173ea463db9
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-09-08 07:38:13 +0530

    Time: 1 ms (83.65%), Space: 42.8 MB (8.32%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,16 +1,5 @@
 class Solution {
     public int countCommas(int n) {
-        int cnt = 0;
-        // if(n>99999){
-        //     cnt += n - 100000 + 1;
-        // }
-        // if(n>9999){
-        //     cnt += n - 10000 + 1;
-        // }
-        // if(n>999){
-        //     cnt += n - 1000 + 1;
-        // }
-        if(n>999) cnt += n - 999;
-        return cnt;
+        return n>999? n - 999:0;
     }
 }
