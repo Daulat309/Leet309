5fe239709bf0ec86aef3558c611bd481b487aa88
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-09-08 11:16:39 +0530

    Time: 2 ms (44.78%), Space: 94.3 MB (85.3%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,22 +1,35 @@
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length;
+
+//         int[] dp = new int[n];
+//         int[] minPrice = new int[n];
+
+//         minPrice[0] = prices[0];
+//         dp[0] = 0;
+
+//         for (int i = 1; i < n; i++) {
+//             minPrice[i] = Math.min(minPrice[i - 1], prices[i]);
+
+//             dp[i] = Math.max(
+//                 dp[i - 1],
+//                 prices[i] - minPrice[i]
+//             );
+//         }
+
+//         return dp[n - 1];
+//     }
+// }
+
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-
-        int[] dp = new int[n];
-        int[] minPrice = new int[n];
-
-        minPrice[0] = prices[0];
-        dp[0] = 0;
-
-        for (int i = 1; i < n; i++) {
-            minPrice[i] = Math.min(minPrice[i - 1], prices[i]);
-
-            dp[i] = Math.max(
-                dp[i - 1],
-                prices[i] - minPrice[i]
-            );
+        int mx = prices[n-1];
+        int res = 0;
+        for(int i = n-2;i>=0;i--){
+            mx = Math.max(mx, prices[i]);
+            res = Math.max(res, mx-prices[i]);
         }
-
-        return dp[n - 1];
+        return res;
     }
 }
