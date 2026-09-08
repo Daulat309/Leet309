9bcff599a215aa23109eee316f64446a2c4f3b01
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-09-08 09:30:49 +0530

    Time: 17 ms (88.92%), Space: 164 MB (31.86%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,32 +1,42 @@
-class Solution {
-    int[][] dp;
-    HashMap<Integer, Integer> mp = new HashMap<>();
-    int n;
-    public boolean canCross(int[] s) {
-        n = s.length;
-        dp = new int[n][n];
-        for(int i = 0;i<n;i++) mp.put(s[i],i);
-        return check(0,0,0,s);
-    }
+class Solution { 
+    int[][] dp; 
+    HashMap<Integer, Integer> mp = new HashMap<>(); 
+    int n; 
 
-    public boolean check(int prev, int i, int p, int[] s){
-        if(i==n-1) return true;
-        boolean res = false;
-        if(dp[prev][i]!=0) return dp[prev][i]==1;
+    public boolean canCross(int[] s) { 
+        n = s.length; 
+        dp = new int[n][n]; 
 
-        for(int k = p - 1;k<=p+1;k++){
-            if(k>0){
-                int nxt = s[i]+k;
-                if(mp.containsKey(nxt)){
-                    if(check(i,mp.get(nxt),k,s)){
-                        dp[i][k] = 1;
-                        res = true;
-                    }
-                }
-                else dp[i][k] = -1;
-            }
+        for(int i = 0; i < n; i++) {
+            mp.put(s[i], i); 
         }
-        dp[prev][i] = res?1:-1;
-        return dp[prev][i]==1;
-    }
+
+        return check(0, 0, s); 
+    } 
+ 
+    public boolean check(int i, int p, int[] s) { 
+        if(i == n - 1) return true; 
+
+        if(dp[i][p] != 0) {
+            return dp[i][p] == 1; 
+        }
+
+        for(int k = p - 1; k <= p + 1; k++) { 
+            if(k > 0) { 
+                int nxt = s[i] + k; 
+
+                if(mp.containsKey(nxt)) { 
+                    int next = mp.get(nxt);
+
+                    if(check(next, k, s)) { 
+                        dp[i][p] = 1;
+                        return true;
+                    }
+                } 
+            } 
+        } 
+
+        dp[i][p] = -1;
+        return false;
+    } 
 }
