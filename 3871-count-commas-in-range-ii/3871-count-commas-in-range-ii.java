25b6869c731bdb0987f33f948339b89fb18f0a6c
Daulat Tikhole <152207966+Daulat309@users.noreply.github.com>
2026-09-09 07:08:39 +0530

    Time: 1 ms (99.36%), Space: 42.6 MB (56.41%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public long countCommas(long n) {
+
+        long cnt = 0;
+
+        if (n > 999) {
+            cnt += n - 999;
+        }
+
+        if (n > 999999) {
+            cnt += n - 999999;
+        }
+
+        if (n > 999999999) {
+            cnt += n - 999999999;
+        }
+
+        if (n > 999999999999L) {
+            cnt += n - 999999999999L;
+        }
+
+        if (n > 999999999999999L) {
+            cnt ++;
+        }
+
+        return cnt;
+    }
+}
